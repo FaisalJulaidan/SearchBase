a61ac03c356c2468c96d2e3123f0e0e3e720764b
The Danger Side GitHub repository <m.esteghamatdar@gmail.com>
2018-03-09 23:53:42 +0000

    404 page updated again

--- app.py
+++ app.py
@@ -328,5 +328,5 @@ def PrivacyPage():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-if __name__ == "__main__":
-	app.run(debug=True)
+# if __name__ == "__main__":
+# 	app.run(debug=True)

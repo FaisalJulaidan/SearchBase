e48a680b4cf06a52450250bd44cdf2c233dbd209
Mehdi Este <m.esteghamatdar@gmail.com>
2018-04-28 23:23:25 +0100

    dfdsf

--- app.py
+++ app.py
@@ -448,35 +448,38 @@ def adminAnswers():
 		user_mail = request.cookies.get("UserEmail")
 		for i in range(1, 13):
 			if(request.form.get("pname" + str(i)) != None):
-				if (request.files['file'+str(i)].filename == ""):
-					print('no file given')
-					if(request.form.get("delPic" + str(i)) != "yes"):
-						cur.execute("SELECT Answer"+str(i)+" FROM \""+user_mail+"\" WHERE Question=\""+selected_question+"\"")
-						data = cur.fetchall()
-						link = data[0][0].split(";")[2]
-						answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";"+link)
+				try:
+					if (request.files['file'+str(i)].filename == ""):
+						print('no file given')
+						if(request.form.get("delPic" + str(i)) != "yes"):
+							cur.execute("SELECT Answer"+str(i)+" FROM \""+user_mail+"\" WHERE Question=\""+selected_question+"\"")
+							data = cur.fetchall()
+							link = data[0][0].split(";")[2]
+							answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";"+link)
+						else:
+							answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";../static/img/core-img/android-icon-72x72.png")
 					else:
-						answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";../static/img/core-img/android-icon-72x72.png")
-				else:
-					file = request.files['file'+str(i)]
-					if file.filename == '':
-						print('No file name')
-					elif file and allowed_file(file.filename):
-						filename = secure_filename(file.filename)
-						filePath = os.path.join(app.config['PRODUCT_IMAGES'], filename)
-					file.save(filePath)
-					filePath = filePath.split("TheSearchBase")[len(filePath.split("TheSearchBase")) - 1]
-					# temporay string
-					tempList = list(filePath)
-					tempString = ""
-					for char in tempList:
-						if(char == "\\"):
-							char = "/"
-						tempString += char
-					filePath = tempString
-					# tempString = filePath.split("\\")
-					# filePath = tempString[0] + "/" + tempString[1]
-					answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";.."+filePath)
+						file = request.files['file'+str(i)]
+						if file.filename == '':
+							print('No file name')
+						elif file and allowed_file(file.filename):
+							filename = secure_filename(file.filename)
+							filePath = os.path.join(app.config['PRODUCT_IMAGES'], filename)
+						file.save(filePath)
+						filePath = filePath.split("TheSearchBase")[len(filePath.split("TheSearchBase")) - 1]
+						# temporay string
+						tempList = list(filePath)
+						tempString = ""
+						for char in tempList:
+							if(char == "\\"):
+								char = "/"
+							tempString += char
+						filePath = tempString
+						# tempString = filePath.split("\\")
+						# filePath = tempString[0] + "/" + tempString[1]
+						answers.append(request.form.get("pname" + str(i))+";"+request.form.get("keywords" + str(i))+";.."+filePath)
+				except:
+					print("file upload failed")
 		c=0
 		for a in answers:
 			c+=1

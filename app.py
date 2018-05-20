637c653970dec85f2f4a71e64c35cad9f4c4ea8f
Luke Warlow <>
2018-05-20 22:04:29 +0100

    Added popup for properties

--- app.py
+++ app.py
@@ -434,13 +434,11 @@ def emojiConterter():
     if request.method == "GET":
         return render_template("emoji-converter.html")
 
-# @app.route("/popup", methods=['GET'])
-# def popup():
-#     if request.method == "GET":
-#         return render_template("pop-test.html")
+@app.route("/popup2", methods=['GET'])
+def popup():
+    if request.method == "GET":
+        return render_template("pop-test.html")
 
-
-# @app.route("/popup2", methods=['GET'])
 @app.route("/popup", methods=['GET'])
 def popup2():
     if request.method == "GET":

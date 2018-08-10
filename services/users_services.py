3221276690bfbb653100006555e3b62ca5dc8cdf
Abdullah Hashim <sombek990@hotmail.com>
2018-08-10 19:35:47 +0300

    prepair login work to be migrated with the new code

--- services/users_services.py
+++ services/users_services.py
@@ -1,7 +1,10 @@
 from models import User
-
+from .db_services import _safeCommit
 
 class UserServices:
 
     def getByID(id):
         return User.query.get(id)
+
+    def getUsers():
+        return User.query.get()

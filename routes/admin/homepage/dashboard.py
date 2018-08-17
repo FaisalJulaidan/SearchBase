dee1ca2b02a89a3a88a06714cfccd2ae68a341b2
Abdullah Hashim <sombek990@hotmail.com>
2018-08-17 15:54:21 +0300

    Fix errors and proper use of isLogged in auth_services

--- routes/admin/homepage/dashboard.py
+++ routes/admin/homepage/dashboard.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
 from services import statistics_services, assistant_services, user_services, auth_services
-from models import Callback
+from models import Callback, User
 
 homepage_router: Blueprint = Blueprint('homepage_router', __name__, template_folder="../../templates")
 
@@ -8,18 +8,18 @@ homepage_router: Blueprint = Blueprint('homepage_router', __name__, template_fol
 # Dashboard page
 @homepage_router.route("/admin/dashboard", methods=['GET'])
 def admin_home():
-    if request.method == "GET" and auth_services.isLogged():
+    if request.method == "GET":
         callback: Callback = user_services.getUserFromSession()
-        user = callback.Data
-        print(user)
-        callback: Callback = statistics_services.getTotalAll(user.Company.Assistants)
         if callback.Success:
-            return render_template("admin/main.html",
+            user: User = callback.Data
+            callback: Callback = statistics_services.getTotalAll(user.Company.Assistants)
+            if callback.Success:
+                return render_template("admin/main.html",
                                        totalClicks=callback.Data.ProductsReturned,
                                        loadedAnswers=callback.Data.QuestionsAnswered,
                                        assistants=assistant_services.getAll(user.CompanyID))
+            else:
+                print(callback.Message)
+                return redirect('login')
         else:
-            print(callback.Message)
             return redirect('login')
-    else:
-        return redirect('login')

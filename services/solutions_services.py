15181a76eb558ea32de79f1b52dcd55cf501e52f
Abdullah Hashim <sombek990@hotmail.com>
2018-08-21 20:28:24 +0300

    evgeniy added a space in solution_services file name and this is why it's not importing

--- services/solutions_services.py
+++ services/solutions_services.py
@@ -7,3 +7,14 @@ def getByAssistantID(assistantID):
     products_callback : Callback = db.session.query(Product).get(assistantID)
 
     return products_callback
+
+def deleteAllByAssistantID(assistantID):
+
+    try:
+        db.session.query(Product).filter(Product.AssistantID == assistantID).delete()
+    except sqlalchemy.exc.SQLAlchemyError as exc:
+        print(exc)
+        db.session.rollback()
+        return False
+
+    return True

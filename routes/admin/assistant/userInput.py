ef9ddd8149aef9a0a9a5ab67832fe45d32085192
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-08-25 17:59:30 +0100

    finished connection route and userInput route. userInput needs testing with db data

--- routes/admin/assistant/userInput.py
+++ routes/admin/assistant/userInput.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, session
-from services import statistics_services, assistant_services
+from services import questions_services, admin_services, userInput_services
 from models import Callback
-from utilties import helpers
 
 userInput_router: Blueprint = Blueprint('userInput_router', __name__ , template_folder="../../templates")
 
@@ -11,29 +10,20 @@ userInput_router: Blueprint = Blueprint('userInput_router', __name__ , template_
 def admin_user_input(assistantID):
 
     if request.method == "GET":
-        email = session.get('User')['Email']
-        assistant = select_from_database_table("SELECT * FROM Assistants WHERE ID=?", [assistantID,])
-        if assistant is None or "Error" in assistant:
-            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            questions = select_from_database_table("SELECT * FROM Questions WHERE AssistantID=?;",
-                                                    [assistantID], True)
-            data = []
-            print("questions: ", questions)
-            #dataTuple = tuple(["Null"])
-            for i in range(0, len(questions)):
-                question = questions[i]
-                print("question: ", question)
-                questionID = question[0]
-                print("questionID: ", questionID)
-                print(query_db("SELECT * FROM UserInput"), [])
-                userInput = select_from_database_table("SELECT * FROM UserInput WHERE QuestionID=?", [questionID], True)
-                print("userInput: ", userInput)
-                if userInput and userInput is not None:
-                    for record in userInput:
-                        print("record: ", record)
-                        data.append(record)
-            print("data:", data)
-            return render("admin/data-storage.html", data=data)
+        question_callback : Callback = questions_services.getByAssistantID(assistantID)
+        if not question_callback.Success : return admin_services.render("admin/data-storage.html")
+
+        data=[]
+
+        for question in question_callback.Data:
+
+            userInput_callback : Callback = userInput_services.getByQuestionID(question.ID)
+            if not userInput_callback.Success : return admin_services.render("admin/data-storage.html")
+
+            userInputData = admin_services.convertForJinja(userInput_callback.Data)
+
+            data.append(userInputData)
+
+        return admin_services.render("admin/data-storage.html", data=data)
 
 

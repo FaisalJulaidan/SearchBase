15489389a144f4eca3a1a1277e63e9ab6908644a
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-08-26 20:52:04 +0100

    changePassword and resetPassword routes done

--- services/mail_services.py
+++ services/mail_services.py
@@ -37,3 +37,22 @@ def sendVerificationEmail(email, companyName, fullname) -> Callback:
         return Callback(False, 'Could not send a verification email to ' + email)
 
     return Callback(True, 'Verification email sent successfully to ' + email)
+
+def sendPasswordResetEmail(email, companyID):
+    try:
+        msg = Message("Password reset",
+                    sender="thesearchbase@gmail.com",
+                    recipients=[email])
+              
+        payload = email + ";" + companyID
+        link = "https://www.thesearchbase.com/account/resetpassword/" + verificationSigner.dumps(payload)
+        msg.html ="<img src='https://thesearchbase.com/static/email_images/password_reset.png' style='width:500px;height:228px;'> <h4> Hi, </h4><p>We have been informed you would like to reset your password. \
+                Please visit <a href='"+link+"'>this link</a> to verify your account and to set your new password.</p> <br /> <br /> \
+                <p>If you have received this by mistake, please let our team know and kindly delete this email</p><br /> Regards, <br /> TheSearchBase Team \
+                <img src='https://thesearchbase.com/static/email_images/footer_image.png' style='width:500px;height:228px;'>"
+        mail.send(msg)
+
+    except Exception as e:
+        return Callback(False, 'Could not send a password reset email to ' + email)
+    
+    return Callback(True, 'Password reset email sent successfully to ' + email)

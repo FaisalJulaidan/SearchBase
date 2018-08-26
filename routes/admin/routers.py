1489a1b4a2f16edf60bb2fcdaf3352a9f45b6aae
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-08-26 20:55:25 +0100

    changes for merge

--- routes/admin/routers.py
+++ routes/admin/routers.py
@@ -3,10 +3,10 @@ from flask import Blueprint
 from .dashboard.dashboard import dashboard_router
 dashboard_router: Blueprint = dashboard_router
 
-from .profile.profile import profile_router
+from .account.profile import profile_router
 profile_router: Blueprint = profile_router
 
-from .profile.changePassword import changePassword_router
+from .account.changePassword import changePassword_router
 changePassword_router: Blueprint = changePassword_router
 
 from .admin_api import admin_api

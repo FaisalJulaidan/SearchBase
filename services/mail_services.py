25fd0d065e04c60b362527d935147d170cf47213
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-09-01 19:59:08 +0100

    added added user email

--- services/mail_services.py
+++ services/mail_services.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer
 from models import Callback
+import string
+import random
 
 mail = Mail()
 verificationSigner = URLSafeTimedSerializer(b'\xb7\xa8j\xfc\x1d\xb2S\\\xd9/\xa6y\xe0\xefC{\xb6k\xab\xa0\xcb\xdd\xdbV')
@@ -62,4 +64,25 @@ def sendPasswordResetEmail(email, companyID):
     
     return Callback(True, 'Password reset email sent successfully to ' + email)
 
+def addedNewUserEmail(adminEmail, targetEmail):
+    try:
+        password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(9))
+        link = "https://www.thesearchbase.com/admin/changepassword"
 
+        msg = Message("You have been added to TheSearchBase",
+                        sender="thesearchbase@gmail.com",
+                        recipients=[targetEmail])
+
+        msg.html = "<h4>Hi, <h4> <br /> <p>You have been registered with TheSearchBase by an admin at your company.<br /> \
+                    To get access to the platform, we have generated a temporary password for you to access the platform.</p> <br /> \
+                    <h4>Your temporary password is: "+password+".<h4><br />\
+                    Please visit <a href='"+link+"'>this link</a> to sign in. To change your password you can use the same link or go to Account Details -> Profile on the left menu.<p><br /> \
+                    If you feel this is a mistake please contact "+adminEmail+". <br /> <br /> Regards, <br />TheSearchBase Team"
+
+        mail.send(msg)
+
+    except:
+        print("addedNewUserEmail() Error: ", e)
+        return Callback(False, 'Could not send email to ' + targetEmail)
+    
+    return Callback(True, 'Password reset email sent successfully to ' + targetEmail)

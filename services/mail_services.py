48682eeef7fc149e83e271dc3f377b7de509bba4
Mehdi Este <m.esteghamatdar@gmail.com>
2018-09-01 20:27:26 +0100

    commit

--- services/mail_services.py
+++ services/mail_services.py
@@ -1,22 +1,30 @@
 import sqlalchemy.exc
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer
 from models import Callback
 import string
 import random
 
-mail = Mail()
 verificationSigner = URLSafeTimedSerializer(b'\xb7\xa8j\xfc\x1d\xb2S\\\xd9/\xa6y\xe0\xefC{\xb6k\xab\xa0\xcb\xdd\xdbV')
 
 # Mail Config
-MAIL_SERVER = 'smtp.gmail.com',
-MAIL_PORT = 465,
-MAIL_USE_SSL = True,
-MAIL_USERNAME = 'thesearchbase@gmail.com',
-MAIL_PASSWORD = 'pilbvnczzdgxkyzy'
+app = Flask(__name__, static_folder='static')
 
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='thesearchbase@gmail.com',
+    MAIL_PASSWORD='pilbvnczzdgxkyzy',
+
+    TESTING = False,
+    MAIL_SUPPRESS_SEND = False
+)
+
+
+mail = Mail(app)
 
 def sendVerificationEmail(email, companyName, fullname) -> Callback:
 
@@ -25,9 +33,9 @@ def sendVerificationEmail(email, companyName, fullname) -> Callback:
                       sender="thesearchbase@gmail.com",
                       recipients=[email])
         payload = email + ";" + companyName
-        link = "http://206.189.122.126/account/verify/" + verificationSigner.dumps(payload)
+        link = "https://www.thesearchbase.com/account/verify/" + verificationSigner.dumps(payload)
         # need to add the links to the email, right now its just a page.
-        msg.html = render_template('/emails/verification.html', link)
+        msg.html = render_template('/emails/verification.html', link = link)
         mail.send(msg)
 
         # sending the registration confirmation email to us
@@ -53,9 +61,9 @@ def sendPasswordResetEmail(email, companyID):
                     recipients=[email])
               
         payload = email + ";" + str(companyID)
-        link = "http://206.189.122.126/account/resetpassword/" + verificationSigner.dumps(payload)
-        # msg.html
-        render_template('/emails/reset-password.html', link)
+        link = "https://www.thesearchbase.com/account/resetpassword/" + verificationSigner.dumps(payload)
+        msg.html = "Hi"
+        #msg.html = render_template('/emails/reset-password.html', link = link) error with '/emails/reset-password.html' missing?
         mail.send(msg)
 
     except Exception as e:

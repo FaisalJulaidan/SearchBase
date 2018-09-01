477f8456f107e45c914006529ca000a68dd9745a
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-09-01 03:24:09 +0300

    Integrate SQLAthanor library to convert SQLAlchemy to JSON and Dict easily

--- routes/admin/dashboard/dashboard.py
+++ routes/admin/dashboard/dashboard.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, redirect, session
-from services import statistics_services, user_services, admin_services, sub_services
+from services import statistics_services, user_services, admin_services, sub_services, company_services, questions_services
 from models import Callback, User, Plan
 
 
@@ -10,8 +10,18 @@ dashboard_router: Blueprint = Blueprint('dashboard_router', __name__, template_f
 def admin_home():
     if request.method == "GET":
         callback: Callback = user_services.getByID(session.get('UserID', 0))
+
+        # c_callback: Callback = company_services.getByID(1)
+        # company = c_callback.Data
+        # json = company.Assistants[0].to_dict(max_nesting=1)
+
+        # q_callback: Callback = questions_services.getByAssistantID(1)
+        # questions = q_callback.Data
+        # print(questions[0].to_dict(max_nesting=2))
+
         if callback.Success:
             user: User = callback.Data
+            print(user.to_json(max_nesting=1))
             callback: Callback = statistics_services.getTotalAll(user.Company.Assistants)
             if callback.Success:
                 return admin_services.render("admin/dashboard.html",

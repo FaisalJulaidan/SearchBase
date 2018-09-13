577dac24b30618c5b5f5b9c1ac3045f0b3c55123
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-09-14 00:49:59 +0100

    new chatbot script done 50%

--- static/chatbot/script.js
+++ static/chatbot/script.js
@@ -1,7 +1,17 @@
-
+﻿
 var assistant = null;
 var blocks = []; // Blocks ordered
+var currentBlock = undefined
 var keywords = [];
+var cancelScroll = false;
+var chatInputDiv = document.getElementById("ChatInputDiv");
+
+//old vars need check if needed
+var oldPos = 0;
+var newPos = 0;
+var params = "";
+var questionsAnswered = 0;
+var collectedInformation = [];
 
 
 // This function will get the blocks from the server by assistantID and set the blocks array to be used to create
@@ -35,11 +45,9 @@ function chatbotInit(assistantID) {
 
 // This start the lunch the chatbot for the first time
 function start() {
-    // 1.Popup the chatbot after assistant.secondsUntilPopup
+    // 1.Show the chatbot welcoming message assistant.message
 
-    // 2.Show the chatbot welcoming message assistant.message
-
-    // 3.Show the first block in the blocks array then render it depends on its type
+    // 2.Show the first block in the blocks array then render it depends on its type
     renderBlock(blocks[0]);
 }
 
@@ -53,12 +61,6 @@ function getBlock(id) {
     }
     // If nothing found return null
     return null
-}
-
-
-// This function what manages the flow of the chatbot conversation depends on the user's interaction with the chatbot.
-function submitMessage() {
-
 }
 
 // This should send the data to the server, data such as keywords.
@@ -118,9 +120,27 @@ function renderBlock(block) {
 // This function will render a block of type Questions with its answers to the chatbot.
 // not finished but just to give an idea of what should be done inside this function
 function renderQuestion(block) {
-    return "<div data-block-id='" + block.id +"' data-block-type='Question'>" + block.content.text +
-        "<div>"+ block.content.answers + "</div>"+
-        "</div>"
+    $("#qAnswers").remove();
+    var answerAppendString = "<div id='qAnswers'>";
+    document.getElementById("textMessage").style.display = "inline-block";
+    document.getElementById("fileUploadDiv").style.display = "none";
+
+    var blockAnswers = block.content.answers;
+    for (var i = 1; i < blockAnswers.length; i++) {
+        answerAppendString += "<a class='answerOptions' id='option" + i + "' onclick=\"submitAnswer('" + toEmoticon(blockAnswers[i].text) + "'," + blockAnswers[i].keywords + ")\">" + toEmoticon(blockAnswers[i].text) + "</a>";
+    }
+    //add skip button
+    //answerAppendString += "<a class='answerOptions' id='option" + i + "' onclick=\"SkipQuestion()\">Skip Question</a>";
+    answerAppendString += "</div>"
+    $("#optionsDiv").append(answerAppendString);
+    chatInputDiv.style = "display:none";
+
+    sendAssistantMessage(block.text, 0)
+
+    checkOutsideElements();
+    if (!cancelScroll) {
+        scrollTo("chatBottom");
+    }
 }
 
 function renderUserInput(block) {
@@ -134,4 +154,163 @@ function renderFileUpload(block) {
 function renderSolutions(block) {
     //Abdullah still did not finish a block of type solutions
     // However reaching to this block means you have to sendData() and get the solutions back
+}
+
+function submitAnswer(message, blockKeywords) {
+    cancelScroll = false;
+
+    if (currentBlock.type == "File Upload") {
+        //needs rework
+        //message = document.getElementById("fileUploadB").value.split("\\")[document.getElementById("fileUploadB").value.split("\\").length - 1];
+        //fileUploads.push(currentFileURL + ":::" + questionID + ":::" + message);
+    }
+
+    if (message == "") {
+        //been submited through Send button click
+        message = document.getElementById("textMessage").value;
+        if (message == "" || message.replace(/ /g, "") == "") {
+            return 0;
+        }
+        $('#textMessage').val("");
+    }
+
+    $("#qAnswers").remove();
+    chatInputDiv.style = "display:none";
+    
+    sendUserMessage(message, 500) //print user's message in the chatbox to appear like he is typing back
+    
+    putThinkingGif(900);
+
+    if (currentBlock.storeInDB) {
+        collectedInformation.push({"questionID": currentBlock.id, "message": message});
+
+        sendAssistantMessage(generateUserInputThanks(), 300)
+
+        putThinkingGif(500);
+    }
+
+    addKeywords(blockKeywords)
+
+    getNextBlock();
+}
+
+function getNextBlock() {
+    var targetBlock = undefined
+    if (currentBlock.action == "Go To Next Block") {
+        var blockNumber = currentBlock.order + 1;
+        targetBlock = blocks[blockNumber];
+    }
+    else if (currentBlock.action == "Go To Specific Block") {
+        for (var i = 0; i < blocks.length; i++) {
+            if (blocks[i].id == currentBlock.blockToGoID) { targetBlock = blocks[i]; }
+        }
+    }
+    else if (currentBlock.action == "Show Solutions") {
+        //send the request to server
+        return 0;
+    }
+
+    if (currentBlock != undefined) {
+        if (currentBlock != targetBlock) {
+            renderBlock(targetBlock);
+        }
+    }
+}
+
+function SetRepeat() {
+    $("#optionsDiv").append("<div style='text-align: center;' onclick=\"Reset()\" id='qAnswers'><a class='answerOptions' style='width:400px;'>Search Again</a></div>");
+    checkOutsideElements();
+}
+
+function Reset() {
+    cancelScroll = false;
+    $("#qAnswers").remove();
+    keywords = [];
+    collectedInformation = [];
+    questionsAnswered = 0;
+    start();
+}
+
+
+// helper functions
+function sendUserMessage(message, waitTime) {
+    $("#messagesContainer").append("<div class='ucDiv'><li id='newMessage' class='userChat'>" + toEmoticon(message) + "</li></div>");
+    animateMessage("#newMessage");
+    document.getElementById('newMessage').id = 'oldMessage';
+    await sleep(waitTime);
+}
+
+function sendAssistantMessage(message, waitTime) {
+    $("#messagesContainer").append("<div class='tsbDiv'><li id='newMessage' class='TSBbot'>" + toEmoticon(message) + "</li></div>");
+    animateMessage("#newMessage");
+    document.getElementById('newMessage').id = 'oldMessage';
+    await sleep(waitTime);
+}
+
+function putThinkingGif(addMiliseconds) {
+    $("#messagesContainer").append("<div class='tsbDiv'><li class='TSBbot' id='thinkingGif'><img src='/static/images/typing.gif'></li></div>")
+    animateMessage("#thinkingGif");
+    await sleep(400 + Math.floor(Math.random() * addMiliseconds));
+    $("#thinkingGif").remove();
+}
+
+function addKeywords(blockKeywords) {
+    var addIn = true;
+
+    for (var c = 0; c < blockKeywords.length; c++) {
+        addIn = true;
+
+        for (var i = 0; i < keywords.length; i++) {
+            if (blockKeywords[c] == keywords[i]) {
+                addIn = false;
+                break;
+            }
+        }
+
+        if (addIn) { keywords.push(blockKeywords[c]); }
+    }
+}
+
+function generateUserInputThanks() {
+    var thanks = "";
+    switch (Math.round(Math.random() * 6)) {
+        case 0:
+            thanks = "Thanks 😁";
+            break;
+        case 1:
+            thanks = "Thank you 😃";
+            break;
+        case 2:
+            thanks = "I'll remember that 😉";
+            break;
+        case 3:
+            thanks = "Just one of the things that make you cool 😎";
+            break;
+        case 4:
+            thanks = "Let me just write it down ✍️😊";
+            break;
+        case 5:
+            thanks = "Give me a second to operate that information 😷";
+            break;
+        case 6:
+            thanks = "Thanks 👊";
+            break;
+    }
+    return thanks
+}
+
+function animateMessage(id) {
+    var marginAnimate = "-=25px";
+    if (id == "#thinkingGif") { marginAnimate = "-=25px"; }
+    else if (id == "#newMessage") { marginAnimate = "-=35px"; }
+    $(id).animate({
+        marginTop: marginAnimate
+    }, "slow");
+    if (!cancelScroll) {
+        scrollTo("chatBottom");
+    }
+}
+
+function toEmoticon(message) {
+    return emojione.toImage(message);
 }

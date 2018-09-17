edb3a797b93359f2fabb133bd23fae7876400f4e
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-09-17 22:46:46 +0100

    Adding assistant in userInput record

--- services/chatbot_services.py
+++ services/chatbot_services.py
@@ -1,14 +1,15 @@
 from services import solutions_services
-from models import db, Callback, UserInput
+from models import db, Callback, UserInput, Assistant
 
 
 
 # Process chatbot data
-def processData(data: dict) -> Callback:
+def processData(assistant: Assistant, data: dict) -> Callback:
     try:
-        # userInput = UserInput(Data=data)
+        userInput = UserInput(Data={'collectedInformation': data['collectedInformation']}, Assistant=assistant)
         db.session.add(userInput)
     except Exception as exc:
+        print(exc)
         db.session.rollback()
         return Callback(False, "An error occurred while processing chatbot data.")
     db.session.commit()

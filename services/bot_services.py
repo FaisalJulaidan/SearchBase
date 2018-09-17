579cd70da8755b4f926d48e3f648160dbe2ad816
FaisalJulaidan <JulaidanFA@cardiff.ac.uk>
2018-09-17 12:28:22 +0100

    Change properties needed to add new blocks

--- services/bot_services.py
+++ services/bot_services.py
@@ -61,10 +61,11 @@ def addBlock(data: dict, assistant: Assistant) -> Callback:
         # Validate submitted block data
         json_utils.validateSchema(data, 'blocks/newBlock.json')
         block = data.get('block')
-        newBlock = Block(Type=BlockType(block['type']), Order=maxOrder + 1, Content=block['content'],
+        newBlock = Block(Type=BlockType(block['type']), Order=maxOrder + 1, Content=None,
                          StoreInDB=block['storeInDB'], Assistant=assistant)
         db.session.add(newBlock)
     except Exception as exc:
+        db.session.rollback()
         print(exc.args[0])
         return Callback(False, 'Error occurred while creating a new Block object', exc.args[0])
 

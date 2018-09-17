c3be9d1aba2d06e22643693f7636046d790f9667
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-09-17 15:24:53 +0100

    Test chatbot

--- static/chatbot/script.js
+++ static/chatbot/script.js
@@ -188,12 +188,12 @@ async function submitAnswer(message, blockKeywords=undefined) {
     if (currentBlock.type == "File Upload") {
         //needs rework
         message = document.getElementById("fileUploadB").value.split("\\")[document.getElementById("fileUploadB").value.split("\\").length - 1];
-        if (!checkFileFormat(message)) {
-            sendUserMessage(message)
-            await sleep(350);
-            sendAssistantMessage("That did not match the allowed file types I've been given. They are " + getAllowedFormatsString() + ".")
-            return 0;
-        }
+        // if (!checkFileFormat(message)) {
+        //     sendUserMessage(message)
+        //     await sleep(350);
+        //     sendAssistantMessage("That did not match the allowed file types I've been given. They are " + getAllowedFormatsString() + ".")
+        //     return 0;
+        // }
         //fileUploads.push(currentFileURL + ":::" + questionID + ":::" + message);
     }
 

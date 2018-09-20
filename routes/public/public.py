6594abb8493f024b65d41cc7e1c665de45a8877c
Mehdi Este <m.esteghamatdar@gmail.com>
2018-09-20 22:48:40 +0100

    quick test

--- routes/public/public.py
+++ routes/public/public.py
@@ -64,13 +64,13 @@ def chatbot(assistantID):
 
         return helpers.jsonResponse(True, 200, "Solution list is here!", {'sessionID': ch_callback.Data.ID, 'solutions':solutions})
 
-@public_router.route("/assistant/pagerequest", methods=['GET'])
-def assistant_pagerequest():
-    if request.method == "GET":
-        print("trying to return file")
-        return send_from_directory('static/user_downloads/', "TSBChatbot.html")
+#@public_router.route("/assistant/pagerequest", methods=['GET'])
+#def assistant_pagerequest():
+#    if request.method == "GET":
+#        print("trying to return file")
+#        return send_from_directory('static/user_downloads/', "TSBChatbot.html")
 
-@app.route("/getpopupsettings/<assistantID>", methods=['GET'])
+@public_router.route("/getpopupsettings/<assistantID>", methods=['GET'])
 def get_pop_settings(assistantID):
     if request.method == "GET":
         assistant_callback: Callback = assistant_services.getByID(assistantID)

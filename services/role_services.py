d006c6dfd175ef6730bd11cc13164fda4257a8a5
FaisalJulaidan <JulaidanFA@cardiff.ac.uk>
2018-10-05 00:13:36 +0100

    few change to role service

--- services/role_services.py
+++ services/role_services.py
@@ -62,13 +62,19 @@ def removeAllByCompany(company: Company) -> Callback:
 
 def getByID(id) -> Role or None:
     try:
-        return Callback(True, 'Role does exist.', db.session.query(Role).get(id))
+        # Get result and check if None then raise exception
+        result = db.session.query(Role).get(id)
+        if not result: raise Exception
+        return Callback(True, 'Role does exist.', result)
     except Exception as e:
         return Callback(False, 'Role with id ' + str(id) + ' does not exist')
 
 
 def getByName(name) -> Role or None:
     try:
-        return Callback(True, 'Role does exist.', db.session.query(Role).filter(Role.Name == name).first())
+        # Get result and check if None then raise exception
+        result = db.session.query(Role).filter(Role.Name == name).first()
+        if not result: raise Exception
+        return Callback(True, 'Role does exist.', result)
     except Exception as e:
         return Callback(False, 'Role ' + str(name) + ' does not exist')

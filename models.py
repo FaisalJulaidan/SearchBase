652674f660d96cfecfd85522afdec51f868655da
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-10-14 03:00:16 +0100

    Added Config column to Assistant table

--- models.py
+++ models.py
@@ -268,6 +268,7 @@ class Assistant(db.Model):
     Message = db.Column(db.String(500), nullable=False)
     TopBarText = db.Column(db.String(64), nullable=False)
     SecondsUntilPopup = db.Column(db.Float, nullable=False, default=0.0)
+    Config = db.Column(JsonEncodedDict, nullable=True)
     Active = db.Column(db.Boolean(), nullable=False, default=False)
 
     # Relationships:

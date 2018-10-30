c045a0ce95c4bfcfeb55c949cf39aa0fe977390f
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-10-30 18:11:35 +0000

    apply button is done. added a future bug fix

--- services/solutions_services.py
+++ services/solutions_services.py
@@ -14,33 +14,6 @@ from lxml import etree
 
 import time #used for checking code execution time
 
-        ## Create a dict with keys as solution ids and values as the number occurrences
-        ## of keywords of each solution in the given keywords list
-        #dic = {}
-        #for s in solutions:
-        #    c = sum(k in keywords for k in s.Keywords.split(','))
-        #    dic[s.ID] = c
-
-        ## Sort dict based on value
-        #dic = dict(sorted(dic.items(), key=lambda x: x[1], reverse=True))
-        #print(dic)
-
-        ## return the first 'max' solutions
-        #count = 1
-        #result = []
-        #for key, value in dic.items():
-        #    for s in solutions:
-        #        if s.ID == key and value !=0:
-        #            # incrementing TimesReturned value for every returned solutions by +1
-        #            s.TimesReturned +=1
-        #            result.append(s)
-        #            break
-        #    if count == max:
-        #        break
-        #    count += 1
-        #    # Save
-        #    db.session.commit()
-
 def getSolutionsSortKey(item):
     result = 0
     for key,value in item["matches"].items():
@@ -208,7 +181,7 @@ def update(solution: Solution, content):
         #         return Callback(False, "keyword doesn't follow the correct format ex. key1,key2...")
 
         # Update solution
-        solution.Content = content
+        solution.Content = replaceIDsWithDataRBD(content)
 
         db.session.commit()
         return Callback(True, 'Solution has been successfully edited.')

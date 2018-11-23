d4ce9b021e4335a864e23f28613a6aa133571d84
Abdullah Hashim <sombek990@hotmail.com>
2018-11-23 22:49:06 +0300

    add react app

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from 'antd/lib/button';
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Button type="primary">Button</Button>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;

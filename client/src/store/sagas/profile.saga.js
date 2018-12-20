6c694783c29119ecadc77f7e3f5ff246f582f72e
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-12-20 20:38:33 +0000

    commented out broken code. will continue locally until i finish

--- client/src/store/sagas/profile.saga.js
+++ client/src/store/sagas/profile.saga.js
@@ -1,26 +1,26 @@
-import {put, takeEvery, all} from 'redux-saga/effects'
-import * as actionTypes from '../actions/actionTypes';
-import {http} from "../../helpers";
-import {profileActions} from "../actions";
-
-function* saveProfileDetails(action) {
-    try {
-        const res = yield http.put(`/admin/profile/profiledetails`, action.profile);
-        yield put(assistantSettingsActions.updateAssistantSettingsSuccess(res.data.msg));
-        return yield put(assistantActions.fetchAssistants())
-    } catch (error) {
-        console.log(error);
-        return yield put(assistantSettingsActions.updateAssistantSettingsFailure(error.response.data));
-    }
-}
-
-function* watchUpdateAssistantSettings() {
-    yield takeEvery(actionTypes.UPDATE_ASSISTANT_SETTINGS_REQUEST, updateAssistantSettings)
-}
-
-
-export function* settingsSage() {
-    yield all([
-        watchUpdateAssistantSettings()
-    ])
-}
+// import {put, takeEvery, all} from 'redux-saga/effects'
+// import * as actionTypes from '../actions/actionTypes';
+// import {http} from "../../helpers";
+// import {profileActions} from "../actions";
+//
+// function* saveProfileDetails(action) {
+//     try {
+//         const res = yield http.put(`/admin/profile/profiledetails`, action.profile);
+//         yield put(assistantSettingsActions.updateAssistantSettingsSuccess(res.data.msg));
+//         return yield put(assistantActions.fetchAssistants())
+//     } catch (error) {
+//         console.log(error);
+//         return yield put(assistantSettingsActions.updateAssistantSettingsFailure(error.response.data));
+//     }
+// }
+//
+// function* watchUpdateAssistantSettings() {
+//     yield takeEvery(actionTypes.UPDATE_ASSISTANT_SETTINGS_REQUEST, updateAssistantSettings)
+// }
+//
+//
+// export function* settingsSage() {
+//     yield all([
+//         watchUpdateAssistantSettings()
+//     ])
+// }

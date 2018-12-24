68adfe9338d8ae2f7ac4ea086a054358e9977a88
Evgeniy Tonchev <evgeniy67@abv.bg>
2018-12-24 19:22:58 +0000

    integration page done

--- client/src/application/Dashboard/Assistants/Assistant/Integration/Integration.js
+++ client/src/application/Dashboard/Assistants/Assistant/Integration/Integration.js
@@ -28,22 +28,35 @@ class Integration extends React.Component {
         });
     }
 
+    componentWillUnmount(){
+        this.removeChatbot();
+    }
+
     handleChange = ({target}) => {
         this.setState({
             [target.name]: target.value
         });
     };
 
+    removeChatbot(){
+        let oldBot = document.getElementById("TheSearchBase_Chatbot");
+        if(oldBot){ oldBot.remove(); }
+        let oldBotScript = document.getElementById("oldBotScript");
+        if(oldBotScript){ oldBotScript.remove(); }
+    }
+
     testIntegration = () => {
+        this.removeChatbot();
         const script = document.createElement("script");
 
         script.src = this.state.source;
         script.async = this.state.async;
         script.defer = this.state.defer;
-        // script.dataName = this.state.dataName;
-        // script.dataID = this.state.dataID;
-        // script.dataIcon = this.state.dataIcon;
-        // script.dataCircle = this.state.dataCircle;
+        script.setAttribute("data-name", this.state.dataName);
+        script.setAttribute("data-id", this.state.dataID);
+        // script.setAttribute("data-icon", this.state.dataIcon);
+        script.setAttribute("data-circle", this.state.dataCircle);
+        script.setAttribute("id", "oldBotScript");
 
         document.body.appendChild(script);
     };
@@ -54,8 +67,9 @@ class Integration extends React.Component {
             window.location.hostname + ":" + window.location.port + "/userdownloads/widget.js"}
                               data-name={this.state.dataName}
                               data-id={this.state.dataID}
-                              data-icon={this.state.dataIcon}
-                              data-circle={this.state.dataCircle} async={this.state.async}
+                              // data-icon={this.state.dataIcon}
+                              data-circle={this.state.dataCircle}
+                              async={this.state.async}
                               defer={this.state.defer}/>);
 
         return (
@@ -68,57 +82,53 @@ class Integration extends React.Component {
 
                 <div style={{height: 'calc(100% - 66px)', display: 'flex'}}>
 
-                    <div style={{margin: 5, width: '45%'}}>
+                    <div style={{margin: 5, width: '45%'}} className={styles.Panel}>
 
-                        <div className={styles.Panel}>
-                            <div className={styles.Header} style={{position: "inherit"}}>
-                                <h3>Choosing your pallet</h3>
-                            </div>
+                        <div className={styles.Header} style={{position: "inherit"}}>
+                            <h3>Choosing your Assistant's looks</h3>
+                        </div>
 
 
-                            <div className={styles.Body}>
+                        <div className={styles.Body}>
 
-                                <p> Currently you can edit the colour settings of your assistant's button.
-                                    Simply pick your preferred ones bellow.</p>
-                                <div style={{display: "-webkit-inline-box"}}>
+                            <p> Currently you can edit the colour setting of your assistant's button.
+                                Simply pick your preferred one bellow.</p>
+                            <div style={{display: "-webkit-inline-box"}}>
 
-                                    <div style={{margin: "10px"}}>
-                                        <input type="color" name="dataIcon" value={this.state.dataIcon}
-                                               onChange={this.handleChange}/>
-                                        <label htmlFor="icon">Icon</label>
-                                    </div>
+                                {/*<div style={{margin: "10px"}}>*/}
+                                    {/*<input type="color" name="dataIcon" value={this.state.dataIcon}*/}
+                                           {/*onChange={this.handleChange}/>*/}
+                                    {/*<label htmlFor="icon">Icon</label>*/}
+                                {/*</div>*/}
 
-                                    <div style={{margin: "10px"}}>
-                                        <input type="color" name="dataCircle" value={this.state.dataCircle}
-                                               onChange={this.handleChange}/>
-                                        <label htmlFor="circle">Circle</label>
-                                    </div>
+                                <div style={{margin: "10px"}}>
+                                    <input type="color" name="dataCircle" value={this.state.dataCircle}
+                                           onChange={this.handleChange}/>
+                                    <label htmlFor="circle">Circle</label>
                                 </div>
-
                             </div>
+
                         </div>
                     </div>
-                    <div style={{margin: 5, width: '55%'}}>
+                    <div style={{margin: 5, width: '55%'}} className={styles.Panel}>
 
-                        <div className={styles.Panel}>
-                            <div className={styles.Header} style={{position: "inherit"}}>
-                                <h3>Connecting your assistant</h3>
-                            </div>
+                        <div className={styles.Header} style={{position: "inherit"}}>
+                            <h3>Connecting your assistant</h3>
+                        </div>
 
 
-                            <div className={styles.Body}>
-                                <p>
-                                    To integrate your assistant, you must paste the pre-made code into any part of your
-                                    HTML
-                                    source code.
-                                </p>
+                        <div className={styles.Body}>
+                            <p>
+                                To integrate your assistant, you must paste the pre-made code into any part of your
+                                HTML
+                                source code.
+                            </p>
 
-                                <textarea value={ReactDOMServer.renderToString(urlPaste)}
-                                          style={{width: "90%", height: "90px", fontWeight: "600"}}
-                                          readOnly/>
-                                {urlPaste}
-                                <Button onClick={this.testIntegration} className={"ant-btn-primary"}>Test Bot</Button>
-                            </div>
+                            <textarea value={ReactDOMServer.renderToString(urlPaste)}
+                                      style={{width: "94%", height: "110px", fontWeight: "600"}}
+                                      readOnly/>
+
+                            <Button onClick={this.testIntegration} className={"ant-btn-primary"}>Test Bot</Button>
                         </div>
                     </div>
                 </div>

bf8c5e6caa99b74a9065cadbfc2460ae21a175ff
Abdullah Hashim <sombek990@hotmail.com>
2019-01-20 03:03:02 +0300

    1- Adding text for homepage 2- change styling in login

--- client/src/application/Dashboard/Home/Home.js
+++ client/src/application/Dashboard/Home/Home.js
@@ -14,12 +14,17 @@ class Home extends React.Component {
             <div style={{height: '100%'}}>
                 <div className={styles.Panel}>
                     <div className={styles.Panel_Header}>
-
+                        <h1>Welcome to the Dashboard</h1>
                     </div>
 
-                    <div className={styles.Panel_Body} style={{overflowY: "auto"}}>
-                        <h1>Welcome to the Dashboard.</h1>
-                        <h3>Use your side navigation to navigate the application.</h3>
+                    <div className={styles.Panel_Body}>
+                        <h1>Welcome!</h1>
+                        <img
+                            src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/hello_aeia.svg"
+                            alt="welcome image"
+                            width={300}/>
+                        <h2>Here you can manage all your bots & see their usage analytics</h2>
+                        <h4>Use your side navigation to navigate the application.</h4>
                     </div>
                 </div>
 

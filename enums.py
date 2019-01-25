50556d8729c2b88ee9cc3e7fd260df8b342867e2
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-01-25 19:00:35 +0000

    Fix None to Ignore

--- enums.py
+++ enums.py
@@ -33,21 +33,21 @@ class DataType(Enum):
     # IMPORTANT: make sure when you edit or add new DataType to change the JSON schemas accordingly.
     # You know just ask Faisal Julaidan before making any changes.
     # ===============================================================================================
-    NoType = {'name': 'None', 'validation': None}
+    NoType = {'name': 'None', 'validation': ValidationType.Ignore.value}
     Email = {'name': 'Email', 'validation': ValidationType.Email.value}
     Telephone = {'name': 'Telephone', 'validation': ValidationType.Telephone.value}
     ContactTime = {'name': 'Contact Time', 'validation': ValidationType.Email.value}
     DesiredSalary = {'name': 'Desired Salary', 'validation': ValidationType.Number.value}
-    Availability = {'name': 'Availability', 'validation': None}
-    DesiredPosition = {'name': 'Desired Position', 'validation': None}
-    DesiredPositionYearsExp = {'name': 'Desired Position Years Exp', 'validation': None}
-    TopSkill = {'name': 'Top Skill', 'validation': None}
-    TopSkillYearsExp = {'name': 'Top Skill Years Exp', 'validation': None}
-    Resume = {'name': 'Resume', 'validation': None}
+    Availability = {'name': 'Availability', 'validation': ValidationType.Ignore.value}
+    DesiredPosition = {'name': 'Desired Position', 'validation': ValidationType.Ignore.value}
+    DesiredPositionYearsExp = {'name': 'Desired Position Years Exp', 'validation': ValidationType.Ignore.value}
+    TopSkill = {'name': 'Top Skill', 'validation': ValidationType.Ignore.value}
+    TopSkillYearsExp = {'name': 'Top Skill Years Exp', 'validation': ValidationType.Ignore.value}
+    Resume = {'name': 'Resume', 'validation': ValidationType.Ignore.value}
     LinkdinURL = {'name': 'Linkdin URL', 'validation': ValidationType.URL.value}
-    DesiredLocation = {'name': 'Desired Location', 'validation': None}
-    EmploymentType = {'name': 'Employment Type', 'validation': None}
-    DesiredHourlyRate = {'name': 'Desired Hourly Rate', 'validation': None}
+    DesiredLocation = {'name': 'Desired Location', 'validation': ValidationType.Ignore.value}
+    EmploymentType = {'name': 'Employment Type', 'validation': ValidationType.Ignore.value}
+    DesiredHourlyRate = {'name': 'Desired Hourly Rate', 'validation': ValidationType.Ignore.value}
 
 
 class UserType(Enum):

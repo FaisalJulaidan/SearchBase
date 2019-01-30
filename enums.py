78221965210e42ade9570341c9274d1b1332a525
Mehdi Este <m.esteghamatdar@gmail.com>
2019-01-30 18:10:40 +0000

    Added the extra data types in for client and removed what was not need for the candidates.

--- enums.py
+++ enums.py
@@ -85,13 +85,13 @@ class DataType(Enum):
     DesiredPosition = {'name': 'Desired Position', 'validation': ValidationType.Ignore.value,
                        'userTypes': [UserType.Candidate.value]}
 
-    DesiredPositionYearsExp = {'name': 'Desired Position Years Exp', 'validation': ValidationType.Ignore.value,
-                               'userTypes': [UserType.Candidate.value]}
+    # DesiredPositionYearsExp = {'name': 'Desired Position Years Exp', 'validation': ValidationType.Ignore.value,
+    #                            'userTypes': [UserType.Candidate.value]}
 
     TopSkills = {'name': 'Top Skills', 'validation': ValidationType.Ignore.value,
                 'userTypes': [UserType.Candidate.value]}
 
-    TopSkillsYearsExp = {'name': 'Top Skills Years Exp', 'validation': ValidationType.Ignore.value,
+    YearsExp = {'name': 'Years Exp', 'validation': ValidationType.Ignore.value,
                         'userTypes': [UserType.Candidate.value]}
 
 
@@ -107,6 +107,15 @@ class DataType(Enum):
 
     # Client
 
+    ClientName = {'name': 'Name of the Client\'s company', 'validation': ValidationType.Name.value,
+            'userTypes': [UserType.Client.value]}
+
+    ClientEmail = {'name': 'Email', 'validation': ValidationType.Email.value,
+             'userTypes': [UserType.Client.value]}
+
+    ClientTelephone = {'name': 'Telephone', 'validation': ValidationType.Telephone.value,
+                 'userTypes': [UserType.Client.value]}
+
     Location = {'name': 'Location', 'validation': ValidationType.Ignore.value,
                      'userTypes': [UserType.Client.value]}
 
@@ -118,6 +127,26 @@ class DataType(Enum):
 
     OfferingEmploymentType = {'name': 'Offering Employment Type', 'validation': ValidationType.Ignore.value,
                       'userTypes': [UserType.Client.value]}
+
+    EssentialSkills = {'name': 'Essential Skills', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
+    EssentialYearsExperience = {'name': 'Essential years of experience', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
+    JobType = {'name': 'Type of Job (Perm/Contract)', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
+    JobSalary = {'name': 'Salary being offered', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
+    JobRate = {'name': 'Contract Rate', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
+
+    JobAvailability = {'name': 'Availability Of the Job', 'validation': ValidationType.Ignore.value,
+                     'userTypes': [UserType.Client.value]}
+
 
     DesiredSkills = {'name': 'Desired Skills', 'validation': ValidationType.Ignore.value,
                      'userTypes': [UserType.Client.value]}

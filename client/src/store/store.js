ca41aba1aa91d4ebe682ea3c4e8fcd90254e2908
c1718515 <TonchevE@cardiff.ac.uk>
2019-02-07 14:49:02 +0000

    added missing import

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
-import {assistantSaga, authSaga, flowSaga, profileSaga, chatbotSessions, solutionsSaga} from './sagas'
+import {assistantSaga, authSaga, flowSaga, profileSaga, chatbotSessions, solutionsSaga, usersManagementSaga} from './sagas'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();

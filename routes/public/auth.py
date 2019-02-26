a673f285e930de41a166243576200e656bfabff9
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-02-26 16:35:36 +0000

    some PEP stuff

--- routes/public/auth.py
+++ routes/public/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for, send_from_directory
+from flask import Blueprint, request, redirect
+from flask_jwt_extended import jwt_refresh_token_required
 from itsdangerous import URLSafeTimedSerializer
+
 from models import Callback
-from services import user_services, auth_services, mail_services, jwt_auth_services
+from services import user_services, jwt_auth_services
 from utilities import helpers
-from flask_jwt_extended import jwt_refresh_token_required, get_jwt_identity
 
 auth_router = Blueprint('auth_router', __name__)
 verificationSigner = URLSafeTimedSerializer(b'\xb7\xa8j\xfc\x1d\xb2S\\\xd9/\xa6y\xe0\xefC{\xb6k\xab\xa0\xcb\xdd\xdbV')
@@ -33,6 +34,7 @@ def refresh():
     else:
         return helpers.jsonResponse(False, 401, "Unauthorised!", callback.Data)
 
+
 @auth_router.route("/signup", methods=['POST'])
 def signup_process():
     if request.method == "POST":
@@ -50,7 +52,8 @@ def verify_account(payload):
             data = verificationSigner.loads(payload)
             email = data.split(";")[0]
             user_callback: Callback = user_services.verifyByEmail(email)
-            if not user_callback.Success: raise Exception(user_callback.Message)
+            if not user_callback.Success:
+                raise Exception(user_callback.Message)
 
             return redirect("/login")
 

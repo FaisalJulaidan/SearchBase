ce768137cf400f12ef4f4f5584b61f37dc42aeac
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-03-06 00:06:20 +0000

    1. Change Admin page title to Dashboard 2. Build react for deployment

---
+++ static/react_app/precache-manifest.5befc1bb74b8e8edd59d635d7b06d950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc90d023029cd272b9a",
+    "url": "/static/react_app/react_static/js/runtime~main.ecc90d02.js"
+  },
+  {
+    "revision": "3305744d2ce260874ebd",
+    "url": "/static/react_app/react_static/js/main.3305744d.chunk.js"
+  },
+  {
+    "revision": "ea75bd2ec69730bcdd07",
+    "url": "/static/react_app/react_static/js/1.ea75bd2e.chunk.js"
+  },
+  {
+    "revision": "3305744d2ce260874ebd",
+    "url": "/static/react_app/react_static/css/main.8c8fa74a.chunk.css"
+  },
+  {
+    "revision": "ea75bd2ec69730bcdd07",
+    "url": "/static/react_app/react_static/css/1.921b7c41.chunk.css"
+  },
+  {
+    "revision": "d076ea7a76ebfbdad355bbdf390a6bd7",
+    "url": "/static/react_app/index.html"
+  },
+  {
+    "revision": "b3eb81a468a52c91a7b3a12b002a6d50",
+    "url": "/static/react_app/1c9f8989b6a927ce632f.worker.js"
+  }
+];

ce09f5dbe88931bd73f120a7b6cb9855704a8dde
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-03-19 01:23:28 +0000

    Ready to deploy

---
+++ static/react_app/precache-manifest.1a3432565e59a7f084162eb29646cd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f319e1bbd0572d48c660",
+    "url": "/static/react_app/react_static/js/runtime~main.f319e1bb.js"
+  },
+  {
+    "revision": "91c9d9d21a2197e8c780",
+    "url": "/static/react_app/react_static/js/main.91c9d9d2.chunk.js"
+  },
+  {
+    "revision": "b73794abaae6c73ea8cd",
+    "url": "/static/react_app/react_static/js/1.b73794ab.chunk.js"
+  },
+  {
+    "revision": "91c9d9d21a2197e8c780",
+    "url": "/static/react_app/react_static/css/main.90c75142.chunk.css"
+  },
+  {
+    "revision": "b73794abaae6c73ea8cd",
+    "url": "/static/react_app/react_static/css/1.32617ce3.chunk.css"
+  },
+  {
+    "revision": "4f2c75258a21d3d499b10cac463e2e23",
+    "url": "/static/react_app/index.html"
+  },
+  {
+    "revision": "1f3e60057d57d9940585d3ef7daf2da0",
+    "url": "/static/react_app/de03d6a42aee3c946edf.worker.js"
+  }
+];

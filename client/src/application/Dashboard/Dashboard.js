db0548125b58506ad3f683dcfe35887c28723ee7
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-04-04 14:15:43 +0100

    commented out pricing page fo real dis time

--- client/src/application/Dashboard/Dashboard.js
+++ client/src/application/Dashboard/Dashboard.js
@@ -153,14 +153,14 @@ class Dashboard extends Component {
                                 <Icon type="profile"/>
                                 Profile
                             </Menu.Item>
-                            <Menu.Item key="billing">
-                                <Icon type="pound"/>
-                                Billing
-                            </Menu.Item>
-                            {/*<Menu.Item key="users-management" style={{fontSize: '9pt'}}>*/}
-                                {/*<Icon type="usergroup-add"/>*/}
-                                {/*Users Management*/}
+                            {/*<Menu.Item key="billing">*/}
+                                {/*<Icon type="pound"/>*/}
+                                {/*Billing*/}
                             {/*</Menu.Item>*/}
+                            <Menu.Item key="users-management" style={{fontSize: '9pt'}}>
+                                <Icon type="usergroup-add"/>
+                                Users Management
+                            </Menu.Item>
                         </SubMenu>
 
                         <Menu.Item key="documentation">

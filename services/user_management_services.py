17e0ff1b8e895a18b22582afcba51c65635550b9
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-04-09 11:43:37 +0100

    minor changes

--- services/user_management_services.py
+++ services/user_management_services.py
@@ -27,7 +27,7 @@ def addAdditionalUser(name, email, role, admin_id):
         # removed empty spaces around the strings and split the full name into first name and surname
         names = name.strip().split(" ")
         email = email.strip()
-        firstname = names[0]
+        first_name = names[0]
         surname = names[-1]
 
         # Check if email is already used
@@ -44,7 +44,7 @@ def addAdditionalUser(name, email, role, admin_id):
         password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(9))
 
         # Create the new user for the company
-        create_callback: Callback = user_services.create(firstname, surname, email, password, "00000000000", 
+        create_callback: Callback = user_services.create(first_name, surname, email, password, "00000000000", 
                                                          adminUser.Company, role_callback.Data, verified=True)
         if not create_callback.Success:
             return Callback(False, create_callback.Message)
@@ -65,7 +65,7 @@ def addAdditionalUser(name, email, role, admin_id):
         return Callback(False, 'Sorry, Could not create the user.')
 
 
-def updateWithPermission(userID, firstname, surname, email, role, admin_id) -> Callback:
+def updateWithPermission(user_id, first_name, surname, email, role, admin_id) -> Callback:
     try:
         # Get the admin user who is logged in and wants to edit.
         user_callback: Callback = user_services.getByID(admin_id)
@@ -78,18 +78,18 @@ def updateWithPermission(userID, firstname, surname, email, role, admin_id) -> C
             return Callback(False, "Please make sure you entered all data correctly and have the necessary permission" +
                                    " to do this action")
 
-        user_callback: Callback = user_services.getByID(userID)
+        user_callback: Callback = user_services.getByID(user_id)
         if not user_callback.Success:
             return Callback(False, "Could not find user's records")
         user: User = user_callback.Data
 
         # Get the role to be assigned for the userToUpdate
-        role_callback: Callback = role_services.getByNameAndCompanyID(role.strip(), user.CompanyID)
+        role_callback: Callback = role_services.getByNameAndCompanyID(role, user.CompanyID)
         if not role_callback.Success:
             return Callback(False, role_callback.Message)
 
         # Update user
-        user.Firstname = firstname.strip()
+        user.Firstname = first_name.strip()
         user.Surname = surname.strip()
         user.Email = email.strip().lower()
         user.Role = role_callback.Data
@@ -104,7 +104,7 @@ def updateWithPermission(userID, firstname, surname, email, role, admin_id) -> C
         return Callback(False, 'Sorry, Could not update the user.')
 
 
-def deleteUserWithPermission(userID, admin_id):
+def deleteUserWithPermission(user_id, admin_id):
     try:
         # Get the admin user who is logged in and wants to delete.
         user_callback: Callback = user_services.getByID(admin_id)
@@ -116,7 +116,7 @@ def deleteUserWithPermission(userID, admin_id):
             return Callback(False, "You're not authorised to delete users")
 
         # Delete the user
-        remove_callback: Callback = user_services.removeByID(userID)
+        remove_callback: Callback = user_services.removeByID(user_id)
         if not remove_callback.Success:
             return Callback(False, remove_callback.Message)
 

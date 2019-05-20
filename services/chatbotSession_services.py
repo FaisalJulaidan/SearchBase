89d7c0f1113993e48c1962b20e3c13531514b314
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-05-20 03:44:29 +0100

    comment our some crm work to be finished later

--- services/chatbotSession_services.py
+++ services/chatbotSession_services.py
@@ -53,11 +53,11 @@ def processSession(assistantHashID, data: dict) -> Callback:
                                         UserType=UserType[data['userType'].replace(" ", "")],
                                         Assistant=assistant)
         # CRM integration
-        if assistant.CRM:
-            crm_callback: Callback = crm_services.processSession(assistant, chatbotSession)
-            if crm_callback.Success:
-                chatbotSession.CRMSynced = True
-            chatbotSession.CRMResponse = crm_callback.Message
+        # if assistant.CRM:
+        #     crm_callback: Callback = crm_services.processSession(assistant, chatbotSession)
+        #     if crm_callback.Success:
+        #         chatbotSession.CRMSynced = True
+        #     chatbotSession.CRMResponse = crm_callback.Message
 
         db.session.add(chatbotSession)
         db.session.commit()

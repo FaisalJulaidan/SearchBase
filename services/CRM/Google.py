0d0458d981b01b2a1cadda90642adc473dc67130
unknown <batublob@gmail.com>
2019-06-15 17:51:06 +0100

    added getToken feature, generates a new token if the expiry has passed else gets the current one from the db

--- services/CRM/Google.py
+++ services/CRM/Google.py
@@ -1,10 +1,44 @@
 from models import Callback, Calendar, db
+from datetime import datetime, timedelta
 import enums
 import requests
 import os
+import json
+import dateutil
+
+#todo - csrf - if necessary
+#todo - need to get company id automatically
 
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+#Gets a new token, and refreshes it if the expiry has passed
+def getToken(type, companyID):
+    cal = db.session.query(Calendar)\
+                    .filter(Calendar.CompanyID == companyID)\
+                    .filter(Calendar.Type == type)\
+                    .first()
+    try:
+        expiry = dateutil.parser.parse(cal.Auth['expiry'])
+        if expiry < datetime.now():
+            resp = requests.post("https://oauth2.googleapis.com/token",
+                                 data={
+                                     'refresh_token': cal.Auth['refresh'],
+                                     'client_secret': os.environ['GOOGLE_CALENDAR_CLIENT_SECRET'],
+                                     'client_id': os.environ['GOOGLE_CALENDAR_CLIENT_ID'],
+                                     'grant_type': 'refresh_token'
+                                 })
+            tokenInfo = {
+                'expiry': (datetime.now() + timedelta(seconds=resp.json()['expires_in'])).isoformat(),
+                'access': resp.json()['access_token'],
+                'refresh': cal.Auth['refresh']
+            }
+            cal.Auth = tokenInfo
+            db.session.save(cal)
+            db.session.commit()
+            return resp.json()['access_token']
+        else:
+            return cal.Auth['access']
+    except Exception as e:
+        print(e)
 
 def authorizeUser(code):
     try:
@@ -16,13 +50,20 @@ def authorizeUser(code):
                                  'redirect_uri': os.environ['GOOGLE_CALENDAR_REDIRECT_URI'],
                                  'grant_type': 'authorization_code'
                              })
+        print(getToken(enums.Calendar.Google, 2))
         if 'error' in resp.json():
             raise Exception(resp.json()['error_description'])
         if 'refresh_token' not in resp.json():
             raise Exception("Please go to https://myaccount.google.com/permissions and reset permissions with our app, then try again")
+
         #need to get company id automatically
-        new = Calendar(Auth=resp.json()['refresh_token'], Type=enums.Calendar.Google, CompanyID=2)
-        db.session.add(new)
+        tokenInfo = {
+            'expiry': (datetime.now() + timedelta(seconds=resp.json()['expires_in'])).isoformat(),
+            'access': resp.json()['access_token'],
+            'refresh': resp.json()['refresh_token']
+        }
+        cal = Calendar(Auth=tokenInfo, Type=enums.Calendar.Google, CompanyID=2)
+        db.session.add(cal)
         db.session.commit()
         return Callback(True, 'User authorized succesfully')
     except Exception as e:

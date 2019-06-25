7179c54d5a485e502669fc92a8c6d9aae7ce537b
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-06-26 00:50:25 +0100

    Fix Notify me every in Assistant Setting page

--- client/src/application/Dashboard/Assistants/Assistant/Assistant.js
+++ client/src/application/Dashboard/Assistants/Assistant/Assistant.js
@@ -159,7 +159,7 @@ class Assistant extends Component {
                     <div className={[styles.Body, 'assistantTabs'].join(' ')}>
                         {!assistant ? <Spin/> :
 
-                            <Tabs defaultActiveKey={'Analytics'} size={"large"} animated={false}
+                            <Tabs defaultActiveKey={'Settings'} size={"large"} animated={false}
                                   onTabClick={this.onTabClick}>
                                 <TabPane tab="Analytics" key="Analytics">
                                     <Analytics assistant={assistant}/>

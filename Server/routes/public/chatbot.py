bec9e1019be585fbb25f3e040fa534476b0a04e4
abdullah <sombek990@hotmail.com>
2019-07-15 01:24:00 +0300

    Fix error

--- Server/routes/public/chatbot.py
+++ Server/routes/public/chatbot.py
@@ -32,6 +32,7 @@ def add_header(r):
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
     r.headers['Cache-Control'] = 'public, max-age=0'
+    r.headers['Access-Control-Allow-Origin'] = '*'
     return r
 
 

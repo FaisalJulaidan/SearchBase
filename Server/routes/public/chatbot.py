b8208c9c600c6bbbe0c99c8bd4f43c747cc05767
abdullah <sombek990@hotmail.com>
2019-07-15 01:34:39 +0300

    test

--- Server/routes/public/chatbot.py
+++ Server/routes/public/chatbot.py
@@ -15,9 +15,50 @@ chatbot_router = Blueprint('chatbot_router', __name__, template_folder="../templ
 CORS(chatbot_router)
 
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 @chatbot_router.route("/widgets/chatbot", methods=['GET'])
 # @helpers.gzipped
-@crossdomain(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@crossdomain(origin='*')
 def get_widget():
     if request.method == "GET":
         return send_from_directory('static/js',
@@ -41,7 +82,7 @@ def get_widget():
 # TO BE REMOVED
 @chatbot_router.route("/widgets/chatbot.js", methods=['GET'])
 # @helpers.gzipped
-@crossdomain(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@crossdomain(origin='*')
 def get_widget_legacy():
     if request.method == "GET":
         return send_from_directory('static/js',

85c393f0f3dd122180340513c61426c47dab52bb
abdullah <sombek990@hotmail.com>
2019-07-15 01:27:28 +0300

    test

--- Server/routes/public/chatbot.py
+++ Server/routes/public/chatbot.py
@@ -22,19 +22,19 @@ def get_widget():
     if request.method == "GET":
         return send_from_directory('static/js',
                                    'loadChatbot.js')
-@chatbot_router.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    r.headers['Access-Control-Allow-Origin'] = '*'
-    r.headers['Access-Control-Allow-Headers'] = '*'
-    return r
+# @chatbot_router.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     r.headers['Access-Control-Allow-Origin'] = '*'
+#     r.headers['Access-Control-Allow-Headers'] = '*'
+#     return r
 
 
 # LEGACY CODE

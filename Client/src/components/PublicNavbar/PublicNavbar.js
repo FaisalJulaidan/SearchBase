6ea1ebe41edb85357f5a7e2e3f31961ae5f6b39a
abdullah <sombek990@hotmail.com>
2019-07-21 23:14:02 +0300

    Direct link company logo fixes

--- Client/src/components/PublicNavbar/PublicNavbar.js
+++ Client/src/components/PublicNavbar/PublicNavbar.js
@@ -5,45 +5,41 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCloud} from '@fortawesome/free-solid-svg-icons'
 import styles from './PublicNavbar.module.less'
 
-const PublicNavbar = ({companyLogo, CompanyName}) => {
-
-    return (
-        <div>
-            <Row>
-                <Col span={8} xs={24} className={styles.Navbar}>
-                    <div style={{display: 'flex'}}>
-                        {
-                            companyLogo ?
-                                <div>
-                                    <img src={companyLogo} alt="company logo" height={40}/>
-                                    <div style={{
-                                        lineHeight: '40px',
-                                        marginLeft: 18,
-                                        color: "#9254de"
-                                    }}>
-                                        {CompanyName}
-                                    </div>
+const PublicNavbar = ({companyLogo, CompanyName}) =>
+    <div>
+        <Row>
+            <Col span={8} xs={24} className={styles.Navbar}>
+                <div style={{display: 'flex'}}>
+                    {
+                        companyLogo ?
+                            <div>
+                                <img
+                                    src={`https://tsb.ams3.digitaloceanspaces.com/production/company_logos/${companyLogo}`}
+                                    alt="company logo" height={40}/>
+                                <div style={{
+                                    lineHeight: '40px',
+                                    marginLeft: 18,
+                                    color: "#9254de"
+                                }}>
+                                    {CompanyName}
                                 </div>
-                                :
-                                <>
-                                    <FontAwesomeIcon size="2x" icon={faCloud} style={{color: '#9254de'}}/>
-                                    <div style={{
-                                        lineHeight: '40px',
-                                        marginLeft: 18,
-                                        color: "#9254de"
-                                    }}>
-                                        TheSearchBase
-                                    </div>
-                                </>
-                        }
-
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    );
-
-};
+                            </div>
+                            :
+                            <>
+                                <FontAwesomeIcon size="2x" icon={faCloud} style={{color: '#9254de'}}/>
+                                <div style={{
+                                    lineHeight: '40px',
+                                    marginLeft: 18,
+                                    color: "#9254de"
+                                }}>
+                                    TheSearchBase
+                                </div>
+                            </>
+                    }
+                </div>
+            </Col>
+        </Row>
+    </div>;
 
 PublicNavbar.propTypes = {
     companyLogo: PropTypes.string,

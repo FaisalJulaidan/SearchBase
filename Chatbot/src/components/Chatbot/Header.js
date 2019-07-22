1d4d2136ca461d7a7529c8c69923dbfeb24330d5
abdullah <sombek990@hotmail.com>
2019-07-22 21:19:25 +0300

    Button fix

--- Chatbot/src/components/Chatbot/Header.js
+++ Chatbot/src/components/Chatbot/Header.js
@@ -50,7 +50,7 @@ const Header = ({ title, logoPath, isDirectLink, resetChatbot, closeWindow }) =>
                         isDirectLink ? '' :
                             <Tooltip title="Close"
                                      getPopupContainer={() => document.getElementById('TheSearchBase_Chatbot')}>
-                                <Button className={['Button'].join(' ')}
+                                <Button className={['Button', 'Danger'].join(' ')}
                                         onClick={closeWindow}
                                         type="danger" shape="circle" icon="close"
                                         style={{ display: isDirectLink ? 'none' : '' }}

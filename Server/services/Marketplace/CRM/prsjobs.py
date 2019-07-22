1c155868318e3884ae256795e832520e68a3d629
benedictuttley <benedictuttley@hotmail.com>
2019-07-22 15:51:25 +0100

    prs login added

--- Server/services/Marketplace/CRM/prsjobs.py
+++ Server/services/Marketplace/CRM/prsjobs.py
@@ -31,17 +31,17 @@ Auth =
  
  
 """
-CLIENT_ID = os.environ['PRSJOBSCLIENT_ID']
+CLIENT_ID = os.environ['PRSJOBS_CLIENT_ID']
 CLIENT_SECRET = os.environ['PRSJOBS_CLIENT_SECRET']
 
 
 def testConnection(auth, companyID):
 
     try:
-        if auth.get("refresh_token"):
-            callback: Callback = retrieveRestToken(auth, companyID)
-        else:
-            callback: Callback = login(auth)
+        print("ATTEMPTING LOGIN")
+        print("auth: ")
+        print(auth)
+        callback: Callback = login(auth)
 
         if not callback.Success:
             raise Exception("Testing failed")
@@ -80,50 +80,3 @@ def login(auth):
     except Exception as exc:
         helpers.logError("Marketplace.CRM.Bullhorn.login() ERROR: " + str(exc))
         return Callback(False, str(exc))
-
-
-def retrieveRestToken(auth, companyID):
-    try:
-        authCopy = dict(auth)
-        headers = {'Content-Type': 'application/json'}
-
-        # use refresh_token to generate access_token and refresh_token
-        url = "https://auth.bullhornstaffing.com/oauth/token?grant_type=refresh_token&refresh_token=" + \
-              authCopy.get("refresh_token") + \
-              "&client_id=" + CLIENT_ID + \
-              "&client_secret=" + CLIENT_SECRET
-        get_access_token = requests.post(url, headers=headers)
-
-        if get_access_token.ok:
-            result_body = json.loads(get_access_token.text)
-            access_token = result_body.get("access_token")
-            authCopy["refresh_token"] = result_body.get("refresh_token")
-        else:
-            raise Exception("CRM not set up properly")
-
-        url = "https://rest.bullhornstaffing.com/rest-services/login?version=*&" + \
-              "access_token=" + access_token
-
-        get_rest_token = requests.put(url, headers=headers)
-        if not get_rest_token.ok:
-            raise Exception("Failure in generating rest_token")
-        result_body = json.loads(get_rest_token.text)
-
-        authCopy["rest_token"] = result_body.get("BhRestToken")
-        authCopy["rest_url"] = result_body.get("restUrl")
-
-        saveAuth_callback: Callback = crm_services.updateByType(CRM.Bullhorn, authCopy, companyID)
-        if not saveAuth_callback.Success:
-            raise Exception(saveAuth_callback.Message)
-
-        return Callback(True, 'Rest Token Retrieved', {
-            "rest_token": authCopy["rest_token"],
-            "rest_url": authCopy.get("rest_url"),
-            "refresh_token": authCopy["refresh_token"]
-        })
-
-    except Exception as exc:
-        db.session.rollback()
-        helpers.logError("Marketplace.CRM.Bullhorn.retrieveRestToken() ERROR: " + str(exc))
-        return Callback(False, str(exc))
-

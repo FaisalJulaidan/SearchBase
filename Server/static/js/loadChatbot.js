dd0aea616824b68fa66c1058b230503fb6f321c3
abdullah <sombek990@hotmail.com>
2019-07-27 21:51:08 +0300

    Transpile loadChatbot to ecma5

--- Server/static/js/loadChatbot.js
+++ Server/static/js/loadChatbot.js
@@ -1,49 +1,49 @@
+"use strict";
+
 function main() {
-    const getLink = (src) => {
-        const {protocol, port, hostname} = window.location;
+    var getLink = function getLink(src) {
+        var _window$location = window.location,
+            protocol = _window$location.protocol,
+            port = _window$location.port,
+            hostname = _window$location.hostname;
+
         // return 'http://localhost:5000' + src;
         return 'https://www.thesearchbase.com' + src;
     };
 
-    let myHeaders = new Headers();
+    var myHeaders = new Headers();
     myHeaders.append('Content-Type', 'text/plain');
     myHeaders.append('Accept', 'application/json');
-
-    const headers = {
+    var headers = {
         method: 'GET',
         mode: 'cors',
         headers: myHeaders
     };
-
-    fetch(getLink("/api/static/widgets/chatbot/asset-manifest.json?NoCache=" + new Date().getTime()), headers)
-        .then(response => response.json())
-        .then(manifest => {
-            // request and get assistant data
-            const scriptTag = document.querySelector('script[data-name="tsb-widget"][data-id]');
-            const isDirectLink = scriptTag.getAttribute('directLink') || '';
-            const assistantID = scriptTag.getAttribute('data-id');
-            const btnColor = scriptTag.getAttribute('data-circle') || '#1890ff';
-
-            const s = document.createElement("script");
-            s.src = getLink(`/api/static/widgets/chatbot${manifest.files['main.js']}`);
-            s.async = true;
-            s.defer = true;
-            s.setAttribute('data-directLink', isDirectLink || '');
-            s.setAttribute('data-name', 'tsb-widget');
-            s.setAttribute('data-id', assistantID);
-            s.setAttribute('data-circle', btnColor);
-            s.setAttribute("id", "oldBotScript");
-
-            document.body.appendChild(s);
-
-            const l = document.createElement("link");
-            l.href = getLink(`/api/static/widgets/chatbot${manifest.files['main.css']}`);
-            l.type = "text/css";
-
-            l.rel = "stylesheet";
-            document.getElementsByTagName("head")[0].appendChild(l);
-        }).catch((error) => {
-        console.log(error)
+    fetch(getLink("/api/static/widgets/chatbot/asset-manifest.json?NoCache=" + new Date().getTime()), headers).then(function (response) {
+        return response.json();
+    }).then(function (manifest) {
+        // request and get assistant data
+        var scriptTag = document.querySelector('script[data-name="tsb-widget"][data-id]');
+        var isDirectLink = scriptTag.getAttribute('directLink') || '';
+        var assistantID = scriptTag.getAttribute('data-id');
+        var btnColor = scriptTag.getAttribute('data-circle') || '#1890ff';
+        var s = document.createElement("script");
+        s.src = getLink("/api/static/widgets/chatbot".concat(manifest.files['main.js']));
+        s.async = true;
+        s.defer = true;
+        s.setAttribute('data-directLink', isDirectLink || '');
+        s.setAttribute('data-name', 'tsb-widget');
+        s.setAttribute('data-id', assistantID);
+        s.setAttribute('data-circle', btnColor);
+        s.setAttribute("id", "oldBotScript");
+        document.body.appendChild(s);
+        var l = document.createElement("link");
+        l.href = getLink("/api/static/widgets/chatbot".concat(manifest.files['main.css']));
+        l.type = "text/css";
+        l.rel = "stylesheet";
+        document.getElementsByTagName("head")[0].appendChild(l);
+    }).catch(function (error) {
+        console.log(error);
     });
 }
 

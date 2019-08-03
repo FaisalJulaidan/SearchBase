38d8973fd1009d16b7b13aa74798f13ec906606c
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-08-03 22:41:56 +0300

    Fixing Alimbic migration

--- Server/models.py
+++ Server/models.py
@@ -53,7 +53,7 @@ class AAAAAAAA(db.Model):
     ID = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
 
     def __repr__(self):
-        return '<Database {}>'.format(self.Name)
+        return '<Database {}>'.format(self.ID)
 
 from schemas.Appointment import Appointment
 Appointment: Appointment = Appointment

91a21e30bdcc9778c09d0e251d31556da5b5c3e4
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-08-07 22:04:42 +0300

    Merge

--- Server/routes/staff/staff.py
+++ Server/routes/staff/staff.py
@@ -9,13 +9,11 @@ staff_router = Blueprint('staff_router', __name__)
 def activate_company(payload):
     if request.method == "GET":
         try:
-            print("ACTIVATE COMAPNY")
             data = helpers.verificationSigner.loads(payload, salt='company-activate-key')
             callback: Callback = company_services.activateCompany(data['companyID'])
-            print(data)
+
             if not callback.Success:
                 raise Exception("Failed to activate company account (id: " + str(data['companyID']) + ")")
-
             return "Company account activated successfully :)"
 
         except Exception as exc:

cef9ff72428e0e0ded26d44ac3a3271c21b81953
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-08-21 00:02:22 +0430

    Fixed Bootstrap (Website) conflict with Antd (Dashboard)

--- Client/src/application/Home/Home.js
+++ Client/src/application/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import './bootstrap.css'
 
 import Routes from './routes'
 import ScrollToTop from "./hoc/scroll-to-top/ScrollToTop";

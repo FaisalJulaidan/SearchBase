757cf3dd39892e836460c342dff51d01fb80fedc
benedictuttley <benedictuttley@hotmail.com>
2019-08-21 21:48:46 +0100

    Added wrappers

--- Server/utilities/helpers.py
+++ Server/utilities/helpers.py
@@ -22,8 +22,8 @@ from itsdangerous import URLSafeTimedSerializer
 from sqlalchemy_utils import Currency
 
 from config import BaseConfig
-from models import db, Assistant, Job, Callback, Role
-from services import flow_services, assistant_services, appointment_services
+from models import db, Assistant, Job, Callback, Role, Company
+from services import flow_services, assistant_services, appointment_services, company_services
 from utilities.enums import Period
 
 # ======== Global Variables ======== #
@@ -393,3 +393,104 @@ def HPrint(message):
     print(message + " - (%s, line %s)" % (filename, info.lineno))
 
 # def csrf():
+
+
+# Check if the plan allows Assistant access
+def AccessAssistantsRequired(func):
+    def wrapper():
+        print('Access Assistants decorator executed')
+        user = get_jwt_identity()['user']
+        callback: Callback = company_services.getByID(user['companyID'])
+        if not callback.Success:
+            return jsonResponse(False, 404, "Not found.", None)
+
+        company: Company = callback.Data
+        print('Access Assistants: ', company.AccessAssistants)
+        if company.AccessAssistants:
+            return func()
+        else:
+            return jsonResponse(False, 401, "Assistants not included in plan", None)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
+# Check if the plan allows Assistant access
+def AccessCampaignsRequired(func):
+    def wrapper():
+        print('Access Campaigns decorator executed')
+        user = get_jwt_identity()['user']
+        callback: Callback = company_services.getByID(user['companyID'])
+        if not callback.Success:
+            return jsonResponse(False, 404, "Not found.", None)
+
+        company: Company = callback.Data
+        print('Access Campaigns: ', company.AccessCampaigns)
+        if company.AccessAssistants:
+            return func()
+        else:
+            return jsonResponse(False, 401, "Campaigns not included in plan", None)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
+# Check if the plan allows appointment access
+def AccessAppointmentsRequired(func):
+    def wrapper():
+        print('Access appointments decorator executed')
+        user = get_jwt_identity()['user']
+        callback: Callback = company_services.getByID(user['companyID'])
+        if not callback.Success:
+            return jsonResponse(False, 404, "Not found.", None)
+
+        company: Company = callback.Data
+        print('Access Appointments: ', company.AccessAppointments)
+        if company.AccessAppointments:
+            return func()
+        else:
+            return jsonResponse(False, 401, "Appointments not included in plan", None)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
+# Check if the plan allows autopilot access
+def AccessAutoPilotRequired(func):
+    def wrapper():
+        print('Access autopilot decorator executed')
+        user = get_jwt_identity()['user']
+        callback: Callback = company_services.getByID(user['companyID'])
+        if not callback.Success:
+            return jsonResponse(False, 404, "Not found.", None)
+
+        company: Company = callback.Data
+        print('Access Autopilot: ', company.AccessAutoPilot)
+        if company.AccessAutoPilot:
+            return func()
+        else:
+            return jsonResponse(False, 401, "Autopilot not included in plan", None)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
+# Check if the plan allows database access
+def AccessDatabasesRequired(func):
+    def wrapper():
+        print('Access database decorator executed')
+        user = get_jwt_identity()['user']
+        callback: Callback = company_services.getByID(user['companyID'])
+        if not callback.Success:
+            return jsonResponse(False, 404, "Not found.", None)
+
+        company: Company = callback.Data
+        print('Access Databases: ', company.AccessDatabases)
+        if company.AccessDatabases:
+            return func()
+        else:
+            return jsonResponse(False, 401, "Databases not included in plan", None)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+

e703ce436edab2118786e79d543f5ecce86e5857
abdullah <sombek990@hotmail.com>
2019-08-24 21:40:12 +0300

    1- Split FormItems into modules 2- Add salary picker block type 3- Add salary range form item

--- Client/src/application/Dashboard/Assistants/Assistant/Flow/Blocks/CardTypes/FormItems/CommonFormItems.js
+++ Client/src/application/Dashboard/Assistants/Assistant/Flow/Blocks/CardTypes/FormItems/CommonFormItems.js
@@ -1,13 +1,11 @@
 import { Button, Cascader, Checkbox, Input, Select, Spin } from 'antd';
 import React from 'react';
-import { onCancel, onSelectAction } from './CardTypesHelpers';
+import { onCancel, onSelectAction } from '../CardTypesHelpers';
 
 const Option = Select.Option;
 const CheckboxGroup = Checkbox.Group;
-const { TextArea } = Input;
 
 // Common Components
-////////////////////////////////////////////
 export const QuestionFormItem = ({ FormItem, layout, getFieldDecorator, block, placeholder }) => {
     return (
         <FormItem label="Question"
@@ -64,21 +62,6 @@ export const DataTypeFormItem = ({ FormItem, layout, getFieldDecorator, options,
     );
 };
 
-export const SkippableFormItem = ({ FormItem, layout, getFieldDecorator, block, setStateHandler, label = 'Skippable', toShow = false }) => (
-    <FormItem label={label} {...layout}
-              extra={'When this is checked users can skip responding'}>
-        {getFieldDecorator('isSkippable', {
-            valuePropName: 'checked',
-            initialValue: block.Skippable ? block.Skippable : toShow
-        })(
-            <Checkbox
-                onChange={(event) => setStateHandler ? setStateHandler({ showSkip: event.target.checked }) : null}>
-                Users can skip answering this question
-            </Checkbox>
-        )}
-    </FormItem>
-);
-
 //TODO: Needs to be checked
 export const StoreInDBFormItem = ({ FormItem, layout, getFieldDecorator, block, blockOptions }) => (
     <FormItem label="Store responses?" {...layout}>
@@ -104,22 +87,6 @@ export const AfterMessageFormItem = ({ FormItem, layout, getFieldDecorator, bloc
             }]
         })(
             <Input placeholder="Ex: Your input is recorded"/>
-        )}
-    </FormItem>
-);
-
-export const RawTextFormItem = ({ FormItem, layout, getFieldDecorator, block, placeholder }) => (
-    <FormItem label="Raw Text"
-              extra="This message will displayed straight after the previous block"
-              {...layout}>
-        {getFieldDecorator('rawText', {
-            initialValue: block.Content.text ? block.Content.text : undefined,
-            rules: [{
-                required: true,
-                message: 'Please input raw text field'
-            }]
-        })(
-            <TextArea placeholder={placeholder} autosize={{ minRows: 2, maxRows: 6 }}/>
         )}
     </FormItem>
 );
@@ -208,116 +175,6 @@ export const ShowGoToGroupFormItem = ({ FormItem, layout, getFieldDecorator, blo
                                         }
                                     )
                                 }</Select>
-                        )
-                    }
-                </FormItem>
-            ) : null
-    );
-};
-
-// Skip Section
-//////////////////////////////////////////////////
-
-export const SkipTextFormItem = ({ FormItem, layout, getFieldDecorator, block, text = 'Skip!' }) => {
-    return (
-        <FormItem label="Button Text"
-                  extra="The above text will be shown in a button"
-                  {...layout}>
-            {getFieldDecorator('SkipText', {
-                initialValue: block.SkipText || text,
-                rules: [{
-                    required: true,
-                    max: 37,
-                    message: 'Text it should not exceed 37 character'
-                }]
-            })(
-                <Input/>
-            )}
-        </FormItem>
-    );
-};
-
-export const SkipFormItem = ({ FormItem, layout, getFieldDecorator, setStateHandler, blockOptions, block, label = 'Skip Action' }) => {
-    return <FormItem label={label} {...layout}>
-        {
-            blockOptions.actions ?
-                getFieldDecorator('SkipAction', {
-                    initialValue: block.SkipAction ? block.SkipAction : undefined,
-                    rules: [{
-                        required: true,
-                        message: 'Please select an action'
-                    }]
-                })(
-                    <Select onSelect={(action) => setStateHandler(onSelectAction(action, true))}
-                            placeholder="The next step after this block">{
-                        blockOptions.actions.map((action, i) =>
-                            <Option key={i} value={action}>{action}</Option>)
-                    }</Select>
-                )
-                : <Spin><Select placeholder="The next step after this block"></Select></Spin>
-        }
-    </FormItem>;
-};
-
-export const ShowGoToBlockSkipFormItem = ({ FormItem, layout, getFieldDecorator, allBlocks, showGoToBlock, block }) => {
-    let currentBlock = block;
-    return (
-        showGoToBlock ?
-            (
-                <FormItem label="Go To Specific Block"
-                          extra="The selected block will be shown after clicking on 'Not Interested' button"
-                          {...layout}>
-                    {
-                        getFieldDecorator('skipBlockToGoID',
-                            {
-                                initialValue: currentBlock.SkipBlockToGoID ? currentBlock.SkipBlockToGoID : undefined,
-                                rules: [{ required: true, message: 'Please select your next block' }]
-                            }
-                        )(
-                            <Select placeholder="The next block to go to">{
-                                allBlocks.map((block, i) => {
-                                    if (block.ID !== currentBlock.ID)
-                                        return <Option key={i} value={block.ID}>
-                                            {`(${block.Type}) ${block.Content.text ? block.Content.text : ''}`}
-                                        </Option>;
-                                })
-                            }</Select>
-                        )
-                    }
-                </FormItem>
-            ) : null
-    );
-};
-
-export const ShowGoToGroupSkipFormItem = ({ FormItem, layout, getFieldDecorator, block, currentGroup, allGroups, showGoToGroup }) => {
-    allGroups = allGroups.filter(group => group.id !== currentGroup.id);
-    const selectedGroup = allGroups.find(group => !!group.blocks.find(groupBlock => block.SkipBlockToGoID === groupBlock?.ID));
-    return (
-        showGoToGroup ?
-            (
-                <FormItem label="Go To Specific Group"
-                          extra="The selected group will start from its first block"
-                          {...layout}>
-                    {
-                        getFieldDecorator('skipBlockToGoIDGroup',
-                            {
-                                initialValue: block && selectedGroup ? selectedGroup.blocks[0].ID : undefined,
-                                rules: [{ required: true, message: 'Please select your next group' }]
-                            }
-                        )(
-                            <Select placeholder="The first next block of a group">{
-                                allGroups.map((group, i) => {
-                                        if (group.blocks[0]) {
-                                            return <Option key={i} value={group.blocks[0].ID}>
-                                                {`${group.name}`}
-                                            </Option>;
-                                        } else
-                                            return <Option disabled key={i} value={group.name}>
-                                                {`${group.name}`}
-                                            </Option>;
-                                    }
-                                )
-                            }</Select>
                         )
                     }
                 </FormItem>
--- Client/src/application/Dashboard/Assistants/Assistant/Flow/Blocks/CardTypes/SalaryPicker.js
+++ Client/src/application/Dashboard/Assistants/Assistant/Flow/Blocks/CardTypes/SalaryPicker.js
@@ -1,70 +1,68 @@
-import React, {Component} from 'react';
-import {Card, Divider, Form} from "antd";
+import React, { Component } from 'react';
+import { Card, Divider, Form } from 'antd';
 
-import {getInitialVariables, initActionType, initActionTypeSkip} from './CardTypesHelpers'
+import { getInitialVariables, initActionType, initActionTypeSkip } from './CardTypesHelpers';
 import {
     ActionFormItem,
     AfterMessageFormItem,
     ButtonsForm,
     DataTypeFormItem,
-    FileTypesFormItem,
+    MinMaxSalary,
     QuestionFormItem,
-    ShowGoToBlockFormItem, ShowGoToBlockSkipFormItem,
-    ShowGoToGroupFormItem, ShowGoToGroupSkipFormItem, SkipFormItem,
-    SkippableFormItem, SkipTextFormItem
-} from './FormItems'
+    ShowGoToBlockFormItem,
+    ShowGoToBlockSkipFormItem,
+    ShowGoToGroupFormItem,
+    ShowGoToGroupSkipFormItem,
+    SkipFormItem,
+    SkippableFormItem,
+    SkipTextFormItem
+} from './FormItems';
 
 const FormItem = Form.Item;
 
-class FileUpload extends Component {
+class SalaryPicker extends Component {
 
     state = {
         showGoToBlock: false,
         showGoToGroup: false,
-
-        showGoToBlockSkip: false,
-        showGoToGroupSkip: false,
-
-        fileTypes: [],
+        showSkip: false
     };
 
     onSubmit = () => this.props.form.validateFields((err, values) => {
+        console.log(err, values);
         if (!err) {
-            const flowOptions = this.props.options.flow;
-            console.log(values);
-            let options = {
-                Type: 'File Upload',
-                StoreInDB: true,
-
-                Skippable: values.isSkippable || false,
-                SkipText: values.SkipText || "Skip!",
-                SkipAction: values.SkipAction || "End Chat",
-                SkipBlockToGoID: values.skipBlockToGoID || values.skipBlockToGoIDGroup || null,
-
-                DataType: flowOptions.dataTypes
-                    .find((dataType) => dataType.name === values.dataType[values.dataType.length-1]),
-                Content: {
-                    text: values.text,
-                    action: values.action,
-                    fileTypes: values.fileTypes,
-                    blockToGoID: values.blockToGoID || values.blockToGoIDGroup || null,
-                    afterMessage: values.afterMessage || ""
-                }
-            };
-
-            if (this.props.handleNewBlock)
-                this.props.handleNewBlock(options);
-            else {
-                // Edit Block
-                options.ID = this.props.modalState.block.ID;
-                this.props.handleEditBlock(options);
-            }
+            // const flowOptions = this.props.options.flow;
+            // let options = {
+            //     Type: 'Raw Text',
+            //     StoreInDB: false,
+            //
+            //     Skippable: values.isSkippable || false,
+            //     SkipText: values.SkipText || 'Skip!',
+            //     SkipAction: values.SkipAction || 'End Chat',
+            //     SkipBlockToGoID: values.skipBlockToGoID || values.skipBlockToGoIDGroup || null,
+            //
+            //     DataType: flowOptions.dataTypes.find((dataType) => dataType.name === 'No Type'),
+            //     Content: {
+            //         text: values.rawText,
+            //         action: values.action,
+            //         blockToGoID: values.blockToGoID || values.blockToGoIDGroup || null
+            //     }
+            // };
+            // console.log(options);
+            //
+            // if (this.props.handleNewBlock)
+            //     this.props.handleNewBlock(options);
+            // else {
+            //     // Edit Block
+            //     options.ID = this.props.modalState.block.ID;
+            //     this.props.handleEditBlock(options);
+            // }
         }
     });
 
     componentWillMount() {
-        const {modalState, options} = this.props;
-        const {block} = getInitialVariables(options.flow, modalState);
+        const { modalState, options } = this.props;
+        const { block } = getInitialVariables(options.flow, modalState);
         this.setState({
             ...initActionType(block, this.props.modalState.allGroups),
             ...initActionTypeSkip(block, this.props.modalState.allGroups),
@@ -72,34 +70,41 @@ class FileUpload extends Component {
         });
     }
 
-
     render() {
-        const {modalState, options, form, handleNewBlock, handleEditBlock, handleDeleteBlock} = this.props;
-        const {blockOptions, block} = getInitialVariables(options.flow , modalState, 'File Upload');
-        const {allGroups, allBlocks, currentGroup, layout} = modalState;
-        const {getFieldDecorator} = form;
-        const {typesAllowed} = blockOptions;
-        const {showSkip} = this.state;
+        const { modalState, options, form, handleNewBlock, handleEditBlock, handleDeleteBlock } = this.props;
+        const { blockOptions, block } = getInitialVariables(options.flow, modalState, 'Raw Text');
+        const { allGroups, allBlocks, currentGroup, layout } = modalState;
+        const { getFieldDecorator } = form;
+
+        const { showSkip, minMaxValidateStatus, errorMsg } = this.state;
 
         const buttons = ButtonsForm(handleNewBlock, handleEditBlock, handleDeleteBlock, this.onSubmit, block);
 
+
         return (
-            <Card style={{width: '100%'}} actions={buttons}>
+            <Card style={{ width: '100%' }} actions={buttons}>
                 <Form layout='horizontal'>
+
                     <QuestionFormItem FormItem={FormItem} block={block}
                                       getFieldDecorator={getFieldDecorator}
                                       layout={layout}
-                                      placeholder="Ex: Please upload your CV"/>
+                                      placeholder="Ex: What is your email?"/>
 
                     <DataTypeFormItem FormItem={FormItem} block={block}
                                       getFieldDecorator={getFieldDecorator}
                                       options={this.props.options}
                                       layout={layout}
-                                      blockType={"File Upload"}/>
+                                      blockType={'Salary Picker'}/>
 
-                    <FileTypesFormItem FormItem={FormItem} typesAllowed={typesAllowed} block={block}
-                                       getFieldDecorator={getFieldDecorator}
-                                       layout={layout}/>
+                    <MinMaxSalary FormItem={FormItem} block={block}
+                                  form={this.props.form}
+                                  getFieldDecorator={getFieldDecorator}
+                                  options={this.props.options}
+                                  layout={layout}/>
+
+                    <AfterMessageFormItem FormItem={FormItem} block={block}
+                                          getFieldDecorator={getFieldDecorator}
+                                          layout={layout}/>
 
                     <ActionFormItem FormItem={FormItem} blockOptions={blockOptions} block={block}
                                     setStateHandler={(state) => this.setState(state)}
@@ -119,26 +124,24 @@ class FileUpload extends Component {
                                            getFieldDecorator={getFieldDecorator}
                                            layout={layout}/>
 
-                    <AfterMessageFormItem FormItem={FormItem} block={block}
-                                          getFieldDecorator={getFieldDecorator}
-                                          layout={layout}/>
-
                     <SkippableFormItem FormItem={FormItem} block={block}
-                                       setStateHandler={(state) => this.setState(state)}
                                        getFieldDecorator={getFieldDecorator}
+                                       setStateHandler={(state) => this.setState(state)}
                                        layout={layout}/>
 
                     {
                         showSkip &&
                         <>
-                            <Divider dashed={true} style={{fontWeight: 'normal', fontSize: '14px'}}>
+                            <Divider dashed={true} style={{ fontWeight: 'normal', fontSize: '14px' }}>
                                 Skip Button
                             </Divider>
+
 
                             <SkipTextFormItem FormItem={FormItem}
                                               layout={layout}
                                               getFieldDecorator={getFieldDecorator}
                                               block={block}/>
+
 
                             <SkipFormItem FormItem={FormItem}
                                           blockOptions={blockOptions}
@@ -166,5 +169,5 @@ class FileUpload extends Component {
     }
 }
 
-export default Form.create()(FileUpload);
+export default Form.create()(SalaryPicker);
 

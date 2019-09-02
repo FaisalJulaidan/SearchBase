0e11ed1e5b7ed37fffa2e07d483d0ba0f972a665
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-09-02 22:17:26 +0100

    bullhorn fix outfixed

--- Server/services/Marketplace/CRM/Bullhorn.py
+++ Server/services/Marketplace/CRM/Bullhorn.py
@@ -58,7 +58,7 @@ def login(auth):
 
         headers = {'Content-Type': 'application/json'}
 
-        access_token_url = "https://auth9.bullhornstaffing.com/oauth/token?" + \
+        access_token_url = "https://auth-emea.bullhornstaffing.com/oauth/token?" + \
                            "&grant_type=authorization_code" + \
                            "&redirect_uri=" + helpers.getDomain(3000) + "/dashboard/marketplace/Bullhorn" + \
                            "&client_id=" + CLIENT_ID + \
@@ -102,7 +102,7 @@ def retrieveRestToken(auth, companyID):
         headers = {'Content-Type': 'application/json'}
 
         # use refresh_token to generate access_token and refresh_token
-        url = "https://auth9.bullhornstaffing.com/oauth/token?grant_type=refresh_token&refresh_token=" + \
+        url = "https://auth-emea.bullhornstaffing.com/oauth/token?grant_type=refresh_token&refresh_token=" + \
               authCopy.get("refresh_token") + \
               "&client_id=" + CLIENT_ID + \
               "&client_secret=" + CLIENT_SECRET

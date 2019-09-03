5fbb55021ae86d9234bc3372c53b5e80a630ab33
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-09-03 21:35:00 +0430

    Google Analytics Fixed! 🤘🤘🤘

--- Client/src/application/Home/Home.js
+++ Client/src/application/Home/Home.js
@@ -7,28 +7,14 @@ import Routes from './routes'
 import {history} from '../../helpers'
 import ScrollToTop from "./hoc/scroll-to-top/ScrollToTop";
 import {store} from "../../store/store";
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-70419779-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 const Home = () => {
-
-    const injectGA = () => {
-        if (typeof window == 'undefined') {
-            return;
-        }
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            window.dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-        gtag('config', 'UA-70419779-2');
-    };
-
     return (
         <Provider store={store}>
                 <Router history={history}>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-70419779-2"/>
-                    <script>{injectGA()}</script>
                     <ScrollToTop>
                         <Routes/>
                     </ScrollToTop>

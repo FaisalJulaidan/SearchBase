1e7908392e72c7a299ab3930e0c26be1742316e5
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-09-26 15:21:10 +0330

    Pricing Page added - finished | Public Pages

--- Client/src/application/Home/component/page/pricing/Pricing.js
+++ Client/src/application/Home/component/page/pricing/Pricing.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import styles from './pricing.module.css';
-import {Card, Col, Container, Row} from "react-bootstrap";
-import {Zoom} from "react-reveal";
+import {Col, Container, Row, Button} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import EmailShare from "react-email-share-link";
 import PricingCard from "./pricing-card/PricingCard";
 import Layout from "../../../hoc/layout/Layout";
 import {ReactComponent as Wave} from "../../../hoc/hero-layout/wave.svg";
 import pricingJSON from './pricing.json';
+import {WEBSITE_TITLE} from "../../../../../constants/config";
 
 const Pricing = () => {
+
+    document.title = "Pricing | " + WEBSITE_TITLE;
 
     let pricing = pricingJSON.map((plan, key) => {
         return (
             <Col key={key}>
-                    <PricingCard title={plan.title} subtitle={plan.subtitle} price={plan.price} items={plan.items}/>
+                <PricingCard title={plan.title} subtitle={plan.subtitle} price={plan.price} items={plan.items}
+                icon={plan.icon}/>
             </Col>
         );
     });
@@ -36,6 +41,26 @@ const Pricing = () => {
                 <Container className={styles.content}>
                     <Row>
                         {pricing}
+                    </Row>
+                    <Row className={styles.row_enterprise}>
+                        <Col md={8}>
+                            <h1>Need more?</h1>
+                        </Col>
+                        <Col md={10} className={styles.desc_col}>
+                            <span>We offer additional enterprise services — if you need features not available in the current plans to help your enterprise requirements get going, please let us know. If you are interested in partnering with us around technology, services, or go-to-market, we’d love to have a conversation!</span>
+                        </Col>
+                        <Col md={8}>
+                            <Link to="/get-started">
+                                <Button variant="light" className={styles.demo_button}>Request a demo</Button>
+                            </Link>
+                            <Button variant="outline-light" className={styles.contact_button}>
+                                <EmailShare email="info@SearchBase.com"
+                                            subject="Contact"
+                                            body="Hi, I would like to talk about...">
+                                    {link => (<a href={link} data-rel="external">Contact us</a>)}
+                                </EmailShare>
+                            </Button>
+                        </Col>
                     </Row>
                 </Container>
             </div>
--- Client/src/application/Home/component/page/pricing/pricing-card/PricingCard.js
+++ Client/src/application/Home/component/page/pricing/pricing-card/PricingCard.js
@@ -3,9 +3,10 @@ import styles from './pricing-card.module.css';
 import PropTypes from "prop-types";
 import {Card} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCloud} from "@fortawesome/free-solid-svg-icons";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faHome,faBuilding,faCity} from "@fortawesome/free-solid-svg-icons";
+library.add(faHome,faBuilding,faCity);
 const PricingCard = (props) => {
 
     let items = props.items?.map((item,key)=>{
@@ -14,14 +15,16 @@ const PricingCard = (props) => {
 
     return (
             <Card className={styles.card}>
-                <Card.Body>
-                    <FontAwesomeIcon className={styles.icon} size="6x" icon={faCloud}/>
+                <Card.Body className={styles.body}>
+                    <FontAwesomeIcon className={styles.icon} size="4x" icon={props.icon}/>
                     <h1 className={styles.title}>{props.title}</h1>
+                    <hr/>
+                    <h1 className={styles.subtitle}>{props.subtitle}</h1>
                     <h4 className={styles.price}>{props.price}</h4>
                     <ul className={styles.list}>
                         {items}
                     </ul>
-                    <Button variant="secondary" className={styles.button} block>Order now</Button>
+                    <Button variant="outline-light" className={styles.button} block>Order now</Button>
                 </Card.Body>
             </Card>
     );

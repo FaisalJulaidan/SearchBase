c2fd916f8d23bb54dfa8b892dc56f0d92dad7ee4
unknown <batublob@gmail.com>
2019-09-28 16:11:57 +0100

    ranges are now validated so there is no overlap

--- Chatbot/src/components/Chatbot/UserInputs/MultiDatePicker.js
+++ Chatbot/src/components/Chatbot/UserInputs/MultiDatePicker.js
@@ -13,7 +13,6 @@ import {faTelegramPlane} from '@fortawesome/free-brands-svg-icons';
 const MultiDatePicker = ({message, submitMessage}) => {
 
     let [settings, setSettings] = useState(false);
-    let [selectedDate, setSelectedDate] = useState(null);
     let [selectedDates, setSelectedDates] = useState({individual: [], range: []})
     let [temporaryRange, setTemporaryRange] = useState({start: null, end: null, reverse: false})
     let [mouseDown, setMouseDown] = useState(null)
@@ -91,17 +90,51 @@ const MultiDatePicker = ({message, submitMessage}) => {
         let first = temporaryRange.start.isAfter(temporaryRange.end) // start is after date
         let compare = temporaryRange.reverse ? temporaryRange.end : temporaryRange.start
         if(Math.abs(date.diff(compare, 'hours')) < 24){
-          dates.individual.push(date)
-          setSelectedDates(dates)
+          dates.individual = individualCheck(date) ? [...dates.individual, date] : dates.individual
         } else {
-          dates.range.push(first ? [temporaryRange.end, temporaryRange.start] : [temporaryRange.start, temporaryRange.end])
-          setSelectedDates(dates)
+          let range = first ? [temporaryRange.end, temporaryRange.start] : [temporaryRange.start, temporaryRange.end]
+          dates.range = rangeCheck(range) ? [...dates.range, range] : dates.range
         }
+        setSelectedDates(dates)
         setTemporaryRange({start: null, end: null, reverse: false})
       } else {
         setTemporaryRange({start: date, end: null, reverse: false})
       }
       setOpen(true)
+    }
+
+    const individualCheck = date => {
+      let valid = true
+      selectedDates.range.forEach(range => {
+        if(date.isBetween(range[0], range[1]) || date.isSame(range[0], 'date') || date.isSame(range[1], 'date')){
+          valid = false
+        }
+      })
+      selectedDates.individual.forEach(ind => {
+        if(date.isSame(ind, 'date')){
+          valid = false
+        }
+      })
+      return valid
+    }
+
+    const rangeCheck = rangeIn => {
+      let valid = true
+      selectedDates.range.forEach(range => {
+         // shorten this maybe lol
+        if(rangeIn[0].isBetween(range[0], range[1]) || rangeIn[0].isSame(range[0], 'date') || rangeIn[0].isSame(range[1], 'date')
+            || rangeIn[1].isBetween(range[0], range[1]) || rangeIn[1].isSame(range[0], 'date') || rangeIn[1].isSame(range[1], 'date') 
+            || range[0].isBetween(rangeIn[0], rangeIn[1]) || range[0].isSame(rangeIn[0], 'date') || range[0].isSame(rangeIn[1], 'date')
+            || range[1].isBetween(rangeIn[0], rangeIn[1]) || range[1].isSame(rangeIn[0], 'date') || range[1].isSame(rangeIn[1], 'date')){
+          valid = false
+        }
+      })
+      selectedDates.individual.forEach(ind => {
+        if(ind.isBetween(rangeIn[0], rangeIn[1]) || ind.isSame(rangeIn[0], 'date') || ind.isSame(rangeIn[1], 'date')){
+          valid = false
+        }
+      })
+      return valid
     }
 
     useEffect(() => {
@@ -133,10 +166,21 @@ const MultiDatePicker = ({message, submitMessage}) => {
     //   setSelectedDates(dates)
     // }
 
+    const selectedDatesToString = () => {
+      let str = ""
+      selectedDates.individual.forEach(ind => {
+        str += `${ind.format("L")},`
+      })
+      selectedDates.range.forEach(range => {
+        str += `${range[0].format("L")}-${range[1].format("L")},`
+      })
+      return str.substr(0, str.length-1)
+    }
+
     const inputOnChangeHandler = () => {
         let text;
-        if (selectedDate)
-            text = selectedDate;  
+        if (selectedDates)
+            text = selectedDatesToString();  
 
         let afterMessage = message.block[flowAttributes.CONTENT][flowAttributes.CONTENT_AFTER_MESSAGE];
         let type = messageTypes.TEXT;
@@ -158,26 +202,26 @@ const MultiDatePicker = ({message, submitMessage}) => {
     return (
         <React.Fragment>
             <div className={'InputContainer'} onClick={() => setOpen(true)}>
-                <AntdDatePicker getCalendarContainer={
-                    () => {
-                        if (document.getElementById('TheSearchBase_Chatbot_Input'))
-                            return document.getElementById('TheSearchBase_Chatbot_Input');
-                        else
-                            return document.getElementById('TheSearchBase_Chatbot')
-                    }}
-                            className={'Datepicker'} suffixIcon={<div/>}
-                            dropdownClassName={'DatepickerCalendar'}
-                            showToday={false}
-                            dateRender={renderDate}
-                            open={open}
-                            // onChange={(e) => {
-                            //     if (e)
-                            //         if (e._isAMomentObject)
-                            //             setSelectedDate(e.format('L'));
-                            //         else
-                            //             setSelectedDate(e.target.value);
-                            // }}
-                />
+              <AntdDatePicker getCalendarContainer={
+                  () => {
+                      if (document.getElementById('TheSearchBase_Chatbot_Input'))
+                          return document.getElementById('TheSearchBase_Chatbot_Input');
+                      else
+                          return document.getElementById('TheSearchBase_Chatbot')
+                  }}
+                          className={'Datepicker'} suffixIcon={<div/>}
+                          dropdownClassName={'DatepickerCalendar'}
+                          showToday={false}
+                          dateRender={renderDate}
+                          open={open}
+                          // onChange={(e) => {
+                          //     if (e)
+                          //         if (e._isAMomentObject)
+                          //             setSelectedDate(e.format('L'));
+                          //         else
+                          //             setSelectedDate(e.target.value);
+                          // }}
+              />
             </div>
             <div className={'Submit'}>
                 <i className={'SendIconActive'} onClick={inputOnChangeHandler}>

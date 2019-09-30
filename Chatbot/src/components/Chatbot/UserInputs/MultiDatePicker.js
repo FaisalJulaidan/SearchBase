e6c72997ac20a3c792c6596c71c1760d879cb3f6
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-09-30 22:48:43 +0300

    Fix styling assistant dropdown select

--- Chatbot/src/components/Chatbot/UserInputs/MultiDatePicker.js
+++ Chatbot/src/components/Chatbot/UserInputs/MultiDatePicker.js
@@ -1,164 +1,168 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 // Constants
 import * as messageTypes from '../../../constants/MessageType';
 import * as flowAttributes from '../../../constants/FlowAttributes';
 // Styles
 import './styles/Inputs.css';
 // Components
-import {DatePicker as AntdDatePicker, Icon, Tooltip} from 'antd';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faTelegramPlane} from '@fortawesome/free-brands-svg-icons';
+import { DatePicker as AntdDatePicker, Icon, Tooltip } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 
 
-const MultiDatePicker = ({message, submitMessage}) => {
+const MultiDatePicker = ({ message, submitMessage }) => {
 
     let [settings, setSettings] = useState(false);
-    let [selectedDates, setSelectedDates] = useState({individual: [], range: []})
-    let [temporaryRange, setTemporaryRange] = useState({start: null, end: null, reverse: false})
-    let [mouseDown, setMouseDown] = useState(null)
-    let [open, setOpen] = useState(false)
+    let [selectedDates, setSelectedDates] = useState({ individual: [], range: [] });
+    let [temporaryRange, setTemporaryRange] = useState({ start: null, end: null, reverse: false });
+    let [mouseDown, setMouseDown] = useState(null);
+    let [open, setOpen] = useState(false);
 
     const addClasses = (date, start, end) => {
-        let startCheck = date.isSame(start, 'date')
-        let endCheck = date.isSame(end, 'date')
-        let firstRangeItem = date.isSame(start.clone().add(1, 'days'), 'date')
-        let lastRangeItem = date.isSame(end.clone().subtract(1, 'days'), 'date')
-        let className = ""
-        className += startCheck ? " start" : "" // start of range
-        className += endCheck ? " end" : ""  // end of range
-        className += firstRangeItem ? " range-start" : "" // start of range
-        className += lastRangeItem ? " range-end" : ""  // end of range
-        className += (date.isBetween(start, end) || date.isBetween(end, start)) && !endCheck && !startCheck ? " range": "" // in range
-        return className
-    }
+        let startCheck = date.isSame(start, 'date');
+        let endCheck = date.isSame(end, 'date');
+        let firstRangeItem = date.isSame(start.clone().add(1, 'days'), 'date');
+        let lastRangeItem = date.isSame(end.clone().subtract(1, 'days'), 'date');
+        let className = '';
+        className += startCheck ? ' start' : ''; // start of range
+        className += endCheck ? ' end' : '';  // end of range
+        className += firstRangeItem ? ' range-start' : ''; // start of range
+        className += lastRangeItem ? ' range-end' : '';  // end of range
+        className += (date.isBetween(start, end) || date.isBetween(end, start)) && !endCheck && !startCheck ? ' range' : ''; // in range
+        return className;
+    };
 
     const renderDate = (curDate, today) => {
-      let className = "antd-date-multi"
-      className += curDate.isSame(today, 'month') ? "" : " fade"
-      let element
-      if(temporaryRange.start && temporaryRange.end){
-          className += addClasses(curDate, temporaryRange.start, temporaryRange.end)
-      }
-
-      if(selectedDates.range.length !== 0){
-        for(let dateRange in selectedDates.range){
-          className += addClasses(curDate, selectedDates.range[dateRange][0], selectedDates.range[dateRange][1])
+        let className = 'antd-date-multi';
+        className += curDate.isSame(today, 'month') ? '' : ' fade';
+        let element;
+        if (temporaryRange.start && temporaryRange.end) {
+            className += addClasses(curDate, temporaryRange.start, temporaryRange.end);
         }
-      }
 
-      for(let date in selectedDates.individual){
-        if(Math.abs(selectedDates.individual[date].diff(curDate, 'hours')) < 23){
-          className += " selected"
+        if (selectedDates.range.length !== 0) {
+            for (let dateRange in selectedDates.range) {
+                className += addClasses(curDate, selectedDates.range[dateRange][0], selectedDates.range[dateRange][1]);
+            }
         }
-      } 
-      element = (<div className={className}><span>{curDate.format("D")}</span></div>)
-      return addEventHandlers(element, curDate)
-    }
+
+        for (let date in selectedDates.individual) {
+            if (Math.abs(selectedDates.individual[date].diff(curDate, 'hours')) < 23) {
+                className += ' selected';
+            }
+        }
+        element = (<div className={className}><span>{curDate.format('D')}</span></div>);
+        return addEventHandlers(element, curDate);
+    };
 
     const addEventHandlers = (element, date) => {
-      return React.cloneElement(element, {onMouseDown: e => dateMouseDown(e, date), onMouseOver: e => dateMouseOver(e, date), onMouseUp: e => dateMouseUp(e, date)})
-    }
+        return React.cloneElement(element, {
+            onMouseDown: e => dateMouseDown(e, date),
+            onMouseOver: e => dateMouseOver(e, date),
+            onMouseUp: e => dateMouseUp(e, date)
+        });
+    };
 
     useEffect(() => {
-      function checkValidParent(e){
-        let valid = false
-        e.path.forEach(node => {
-          if(node.classList){
-            if(node.classList.contains("DatepickerCalendar")){
-              valid = true;
+        function checkValidParent(e) {
+            let valid = false;
+            e.path.forEach(node => {
+                if (node.classList) {
+                    if (node.classList.contains('DatepickerCalendar')) {
+                        valid = true;
+                    }
+                }
+            });
+            if (!valid && open) {
+                setOpen(false);
             }
-          }
-        })
-        if(!valid && open){
-          setOpen(false)
         }
-      }
-      if(open){
-        console.log('ree')
-        window.addEventListener("click", checkValidParent)
-      }
-      return () => window.removeEventListener("click", checkValidParent)
-    }, [setOpen, open])
-    
+
+        if (open) {
+            console.log('ree');
+            window.addEventListener('click', checkValidParent);
+        }
+        return () => window.removeEventListener('click', checkValidParent);
+    }, [setOpen, open]);
 
 
     const dateMouseDown = (e, date) => {
-      setMouseDown(date)
-      if(temporaryRange.start){
-        // click same day
-        let dates = Object.assign({}, selectedDates)
-        let first = temporaryRange.start.isAfter(temporaryRange.end) // start is after date
-        let compare = temporaryRange.reverse ? temporaryRange.end : temporaryRange.start
-        if(Math.abs(date.diff(compare, 'hours')) < 24){
-          dates.individual = individualCheck(date) ? [...dates.individual, date] : dates.individual
+        setMouseDown(date);
+        if (temporaryRange.start) {
+            // click same day
+            let dates = Object.assign({}, selectedDates);
+            let first = temporaryRange.start.isAfter(temporaryRange.end); // start is after date
+            let compare = temporaryRange.reverse ? temporaryRange.end : temporaryRange.start;
+            if (Math.abs(date.diff(compare, 'hours')) < 24) {
+                dates.individual = individualCheck(date) ? [...dates.individual, date] : dates.individual;
+            } else {
+                let range = first ? [temporaryRange.end, temporaryRange.start] : [temporaryRange.start, temporaryRange.end];
+                dates.range = rangeCheck(range) ? [...dates.range, range] : dates.range;
+            }
+            setSelectedDates(dates);
+            setTemporaryRange({ start: null, end: null, reverse: false });
         } else {
-          let range = first ? [temporaryRange.end, temporaryRange.start] : [temporaryRange.start, temporaryRange.end]
-          dates.range = rangeCheck(range) ? [...dates.range, range] : dates.range
+            setTemporaryRange({ start: date, end: null, reverse: false });
         }
-        setSelectedDates(dates)
-        setTemporaryRange({start: null, end: null, reverse: false})
-      } else {
-        setTemporaryRange({start: date, end: null, reverse: false})
-      }
-      setOpen(true)
-    }
+        setOpen(true);
+    };
 
     const individualCheck = date => {
-      let valid = true
-      selectedDates.range.forEach(range => {
-        if(date.isBetween(range[0], range[1]) || date.isSame(range[0], 'date') || date.isSame(range[1], 'date')){
-          valid = false
-        }
-      })
-      selectedDates.individual.forEach(ind => {
-        if(date.isSame(ind, 'date')){
-          valid = false
-        }
-      })
-      return valid
-    }
+        let valid = true;
+        selectedDates.range.forEach(range => {
+            if (date.isBetween(range[0], range[1]) || date.isSame(range[0], 'date') || date.isSame(range[1], 'date')) {
+                valid = false;
+            }
+        });
+        selectedDates.individual.forEach(ind => {
+            if (date.isSame(ind, 'date')) {
+                valid = false;
+            }
+        });
+        return valid;
+    };
 
     const rangeCheck = rangeIn => {
-      let valid = true
-      selectedDates.range.forEach(range => {
-         // shorten this maybe lol
-        if(rangeIn[0].isBetween(range[0], range[1]) || rangeIn[0].isSame(range[0], 'date') || rangeIn[0].isSame(range[1], 'date')
-            || rangeIn[1].isBetween(range[0], range[1]) || rangeIn[1].isSame(range[0], 'date') || rangeIn[1].isSame(range[1], 'date') 
-            || range[0].isBetween(rangeIn[0], rangeIn[1]) || range[0].isSame(rangeIn[0], 'date') || range[0].isSame(rangeIn[1], 'date')
-            || range[1].isBetween(rangeIn[0], rangeIn[1]) || range[1].isSame(rangeIn[0], 'date') || range[1].isSame(rangeIn[1], 'date')){
-          valid = false
-        }
-      })
-      selectedDates.individual.forEach(ind => {
-        if(ind.isBetween(rangeIn[0], rangeIn[1]) || ind.isSame(rangeIn[0], 'date') || ind.isSame(rangeIn[1], 'date')){
-          valid = false
-        }
-      })
-      return valid
-    }
+        let valid = true;
+        selectedDates.range.forEach(range => {
+            // shorten this maybe lol
+            if (rangeIn[0].isBetween(range[0], range[1]) || rangeIn[0].isSame(range[0], 'date') || rangeIn[0].isSame(range[1], 'date')
+                || rangeIn[1].isBetween(range[0], range[1]) || rangeIn[1].isSame(range[0], 'date') || rangeIn[1].isSame(range[1], 'date')
+                || range[0].isBetween(rangeIn[0], rangeIn[1]) || range[0].isSame(rangeIn[0], 'date') || range[0].isSame(rangeIn[1], 'date')
+                || range[1].isBetween(rangeIn[0], rangeIn[1]) || range[1].isSame(rangeIn[0], 'date') || range[1].isSame(rangeIn[1], 'date')) {
+                valid = false;
+            }
+        });
+        selectedDates.individual.forEach(ind => {
+            if (ind.isBetween(rangeIn[0], rangeIn[1]) || ind.isSame(rangeIn[0], 'date') || ind.isSame(rangeIn[1], 'date')) {
+                valid = false;
+            }
+        });
+        return valid;
+    };
 
     useEffect(() => {
-      console.log(selectedDates)
-    }, [selectedDates])
+        console.log(selectedDates);
+    }, [selectedDates]);
 
     const dateMouseUp = (e, date) => {
-      if(mouseDown){
-        setMouseDown(null)
-        // setOpen(false)
-      } 
-    }
+        if (mouseDown) {
+            setMouseDown(null);
+            // setOpen(false)
+        }
+    };
 
     const dateMouseOver = (e, date) => {
-      if(temporaryRange.start){
-        if(!temporaryRange.start.isAfter(date) && !temporaryRange.reverse){
-          setTemporaryRange(obj => ({...obj, end: date, reverse: false}))
-        } else if(!temporaryRange.reverse) {
-          setTemporaryRange(obj => ({end: obj.start, start: date, reverse: true}))
-        } else {
-          setTemporaryRange(obj => ({...obj, start: date, reverse: true}))
+        if (temporaryRange.start) {
+            if (!temporaryRange.start.isAfter(date) && !temporaryRange.reverse) {
+                setTemporaryRange(obj => ({ ...obj, end: date, reverse: false }));
+            } else if (!temporaryRange.reverse) {
+                setTemporaryRange(obj => ({ end: obj.start, start: date, reverse: true }));
+            } else {
+                setTemporaryRange(obj => ({ ...obj, start: date, reverse: true }));
+            }
         }
-      }
-    }
+    };
 
     // const selectDate = (date) => {
     //   let dates = Object.assign({}, selectedDates)
@@ -167,20 +171,20 @@ const MultiDatePicker = ({message, submitMessage}) => {
     // }
 
     const selectedDatesToString = () => {
-      let str = ""
-      selectedDates.individual.forEach(ind => {
-        str += `${ind.format("L")},`
-      })
-      selectedDates.range.forEach(range => {
-        str += `${range[0].format("L")}-${range[1].format("L")},`
-      })
-      return str.substr(0, str.length-1)
-    }
+        let str = '';
+        selectedDates.individual.forEach(ind => {
+            str += `${ind.format('L')},`;
+        });
+        selectedDates.range.forEach(range => {
+            str += `${range[0].format('L')}-${range[1].format('L')},`;
+        });
+        return str.substr(0, str.length - 1);
+    };
 
     const inputOnChangeHandler = () => {
         let text;
         if (selectedDates)
-            text = selectedDatesToString();  
+            text = selectedDatesToString();
 
         let afterMessage = message.block[flowAttributes.CONTENT][flowAttributes.CONTENT_AFTER_MESSAGE];
         let type = messageTypes.TEXT;
@@ -202,26 +206,26 @@ const MultiDatePicker = ({message, submitMessage}) => {
     return (
         <React.Fragment>
             <div className={'InputContainer'} onClick={() => setOpen(true)}>
-              <AntdDatePicker getCalendarContainer={
-                  () => {
-                      if (document.getElementById('TheSearchBase_Chatbot_Input'))
-                          return document.getElementById('TheSearchBase_Chatbot_Input');
-                      else
-                          return document.getElementById('TheSearchBase_Chatbot')
-                  }}
-                          className={'Datepicker'} suffixIcon={<div/>}
-                          dropdownClassName={'DatepickerCalendar'}
-                          showToday={false}
-                          dateRender={renderDate}
-                          open={open}
-                          // onChange={(e) => {
-                          //     if (e)
-                          //         if (e._isAMomentObject)
-                          //             setSelectedDate(e.format('L'));
-                          //         else
-                          //             setSelectedDate(e.target.value);
-                          // }}
-              />
+                <AntdDatePicker getCalendarContainer={
+                    () => {
+                        if (document.getElementById('TheSearchBase_Chatbot_Input'))
+                            return document.getElementById('TheSearchBase_Chatbot_Input');
+                        else
+                            return document.getElementById('TheSearchBase_Chatbot');
+                    }}
+                                className={'Datepicker'} suffixIcon={<div/>}
+                                dropdownClassName={'DatepickerCalendar'}
+                                showToday={false}
+                                dateRender={renderDate}
+                                open={open}
+                    // onChange={(e) => {
+                    //     if (e)
+                    //         if (e._isAMomentObject)
+                    //             setSelectedDate(e.format('L'));
+                    //         else
+                    //             setSelectedDate(e.target.value);
+                    // }}
+                />
             </div>
             <div className={'Submit'}>
                 <i className={'SendIconActive'} onClick={inputOnChangeHandler}>

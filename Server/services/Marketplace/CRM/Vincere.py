ca7349a40b712345eb3856881e86dcb97a252aeb
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-10-02 21:13:07 +0100

    vincere test

--- Server/services/Marketplace/CRM/Vincere.py
+++ Server/services/Marketplace/CRM/Vincere.py
@@ -125,7 +125,8 @@ def sendQuery(auth, query, method, body, companyID, optionalParams=None):
 
         # set headers
         headers = {'Content-Type': 'application/json'}
-
+        helpers.logError("BODY: ")
+        helpers.logError(body)
         # test the BhRestToken (rest_token)
         r = marketplace_helpers.sendRequest(url, method, headers, json.dumps(body))
 

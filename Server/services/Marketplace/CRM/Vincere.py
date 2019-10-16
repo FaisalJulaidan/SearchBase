f45504680d2fb63074c30e1ce928373e32d9929f
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-10-16 01:30:37 +0100

    fix

--- Server/services/Marketplace/CRM/Vincere.py
+++ Server/services/Marketplace/CRM/Vincere.py
@@ -469,7 +469,7 @@ def searchJobs(auth, companyID, data) -> Callback:
     try:
         query = "q="
 
-        fields = "fl=id,job_title,public_description,owners,open_date,salary_from,salary_to,employment_type,location"
+        fields = "fl=id,job_title,public_description,owners,open_date,salary_from,salary_to,employment_type,location,currency"
 
         # populate filter
         # query += populateFilter(data.get("jobTitle"), "job_title")

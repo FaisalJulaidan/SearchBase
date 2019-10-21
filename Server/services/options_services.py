a29dc52b948461655a984ce6cd187684f3184ee3
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-10-21 21:17:49 +0300

    Add Referral template

--- Server/services/options_services.py
+++ Server/services/options_services.py
@@ -82,6 +82,7 @@ def getOptions(industry=None) -> Callback:
                     {'label': 'Main Website', 'fileName': 'main'},
                     {'label': 'Appointment Scheduler', 'fileName': 'appointment'},
                     {'label': 'Join Us', 'fileName': 'join-us'},
+                    {'label': 'Referral', 'fileName': 'referral'},
                     {'label': 'Template 1', 'fileName': 'template-1'},
                 ]
             }

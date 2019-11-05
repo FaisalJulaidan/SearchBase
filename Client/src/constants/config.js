c0ab166927e0dce4d1425662b959f6b479a67a3e
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-11-05 21:15:15 +0330

    Changed stripe key from test to live.

--- Client/src/constants/config.js
+++ Client/src/constants/config.js
@@ -2,7 +2,6 @@ const WEBSITE_TITLE = "SearchBase - Recruitment Automation with Chatbots";
 const CURRENT_YEAR = (new Date()).getFullYear();
 const STRIPE_PK = 'pk_live_tjeUzeDhPJpmBaQVBHhk0vQA00J7vNDCb7';
 
-
 export {
     WEBSITE_TITLE,
     CURRENT_YEAR,

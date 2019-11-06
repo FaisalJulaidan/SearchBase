2bc457f7dfec75d2c03c18773eeb8bdad36e5c42
unknown <batublob@gmail.com>
2019-11-06 11:55:52 +0000

    added domain for recruitbot.ai onto helper func

--- Server/services/campaign_services.py
+++ Server/services/campaign_services.py
@@ -221,7 +221,7 @@ def sendCampaign(campaign_details, companyID):
             access = helpers.verificationSigner.dumps({"candidateID": candidate.get("ID"), "source": source, "crmID": crmID}, salt='crm-information')
 
             url : Callback = url_services.createShortenedURL(helpers.getDomain(3000) + "/chatbot_direct_link/" + \
-               hashedAssistantID + "?source=" + str(access))
+               hashedAssistantID + "?source=" + str(access), domain="recruitbot.ai")
             
             if not url.Success:
                 raise Exception("Failed to create shortened URL")

9c3cb7004f69bda4b2360cd25f49e1f326612916
unknown <batublob@gmail.com>
2019-11-06 12:19:39 +0000

    merge

--- Server/utilities/enums.py
+++ Server/utilities/enums.py
@@ -65,6 +65,31 @@ class Status(Enum):
     @classmethod
     def has_value(cls, value):
         return any(value == item.value for item in cls)
+<<<<<<< Updated upstream
+=======
+                
+def planCreator(planID: str, assistants: int = 0, campaigns: int = 0, autopilot: int = 0, databases: int = 0, appointments: int = 0):
+    return {
+      'planID': planID,
+      'accessAssistants': assistants,
+      'accessCampaigns': campaigns,
+      'accessAutopilot': autopilot,
+      'accessDatabases': databases,
+      'accessAppointments'  : appointments }
+
+@unique
+class Plan(Enum):
+    Essential = planCreator("plan_G7Sth78cbr8Pgl", 1, 0, 0, 1, 0)
+    Pro = planCreator("plan_G7SuTtSoBxJ7aS", 1, 1, 0, 1, 1)
+    Premium = planCreator("plan_G7SuT5aJA1OFJU", 1, 1, 1, 1, 1)
+
+    @classmethod
+    def get_plan(cls, value):
+        for item in cls:
+            if(value == item.value['planID']):
+                return item
+        return None
+>>>>>>> Stashed changes
 
 
 @unique

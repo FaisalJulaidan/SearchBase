b9dad0d90c540ca876e6db407ff97c94b90ae849
Evgeniy Tonchev <evgeniy67@abv.bg>
2019-11-06 13:26:52 +0000

    fix

--- Server/services/Marketplace/CRM/Vincere.py
+++ Server/services/Marketplace/CRM/Vincere.py
@@ -533,6 +533,12 @@ def searchJobs(auth, companyID, data) -> Callback:
         result = []
         # not found match for JobLinkURL
         for record in return_body["result"]["items"]:
+            currency = record.get("currency", "gbp").lower()
+            if currency == "pound":
+                currency = "GBP"
+            else:
+                currency = record.get("currency", "GBP").upper()
+
             result.append(databases_services.createPandaJob(id=record.get("id"),
                                                             title=record.get("job_title"),
                                                             desc=record.get("public_description", ""),

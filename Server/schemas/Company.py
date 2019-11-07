568987d282f489fe08a1b0ad08e68327f626b3da
unknown <batublob@gmail.com>
2019-11-07 21:05:37 +0000

    test

--- Server/schemas/Company.py
+++ Server/schemas/Company.py
@@ -26,7 +26,7 @@ class Company(db.Model):
 
     HideSignature = db.Column(db.Boolean, nullable=False, default=False)
     Active = db.Column(db.Boolean, nullable=False, default=False)
-    # Plan = db.Column(Enum(enums.Plan), server_default=enums.Plan.Basic.name)
+    Plan = db.Column(Enum(enums.Plan), server_default=enums.Plan.Basic.name)
 
     # Relationships:
     Users = db.relationship('User', back_populates='Company')

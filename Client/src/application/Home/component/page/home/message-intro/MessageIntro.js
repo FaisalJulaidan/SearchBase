9a2c1517c575da9a01e6ebc8228404c71df7a728
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-11-09 11:27:10 +0330

    Improve HomePage design

--- Client/src/application/Home/component/page/home/message-intro/MessageIntro.js
+++ Client/src/application/Home/component/page/home/message-intro/MessageIntro.js
@@ -14,10 +14,10 @@ class MessageIntro extends React.Component {
         text: {
             intro: 'Seamless Experience',
             title: 'Capture candidates, in seconds',
-            text: 'By interacting with candidates using Chatbots,\n' +
-                'you are working at times of Convenience for the Candidates, in a Confidential\n' +
-                'Environment. With direct interaction between you and candidates, your business can\n' +
-                'make Connections and build Highly Descriptive Candidate Profiles in Seconds.',
+            text: 'By interacting with candidates using chatbots,\n' +
+                'you are working at times of convenience for the candidates, in a confidential\n' +
+                'environment. With direct interaction between you and candidates, your business can\n' +
+                'make connections and build highly descriptive candidate profiles in seconds.',
         }
     };
 
@@ -42,7 +42,7 @@ class MessageIntro extends React.Component {
                                 <h3 className={styles.title}>{this.state.text.title}</h3>
                                 <h6 className={styles.text}>{this.state.text.text}</h6>
                                 <Link to="/how-it-works" className={styles.button}>
-                                    Read How it works <FontAwesomeIcon className={styles.icon}
+                                    How it works <FontAwesomeIcon className={styles.icon}
                                                                        icon={faArrowCircleRight}/>
                                 </Link>
                             </Fade>

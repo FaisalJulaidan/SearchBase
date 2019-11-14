9fb58619bfb843073056ab463b6b561b83ea40b2
Shayan Khaksar <sh.khaksar95@gmail.com>
2019-11-14 17:01:24 +0330

    Completed ROI design.

--- Client/src/application/Home/component/page/roi/ROI.js
+++ Client/src/application/Home/component/page/roi/ROI.js
@@ -1,68 +1,137 @@
 import React from 'react';
 import styles from './roi.module.css'
-import {Col, Container, Row, Card} from "react-bootstrap";
+import {Button, Col, Container, Row, Card} from "react-bootstrap";
+import {Link} from "react-router-dom";
 import Layout from "../../../hoc/layout/Layout";
 import {Input, Icon} from 'antd';
 import ResultItem from "./result-item/ResultItem";
 
-const ROI = () => {
-    return (
-        <Layout>
-            <Container className={styles.container}>
-                <Row>
-                    <Col md={{span: 8, offset: 2}} className={styles.col_intro}>
-                        <h1>What could you save with SearchBase?</h1>
-                        <hr/>
-                        <h4>Use our super simple ROI calculator below to learn more</h4>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={{span: 10, offset: 1}} className={styles.col_card}>
-                        <Card className={styles.card}>
-                            <Container>
-                                <Row className={styles.row_input}>
-                                    <Col>
-                                        <span className={styles.input_title}>staff number</span>
-                                        <Input className={styles.input} size="large" placeholder="50"/>
-                                    </Col>
-                                    <Col>
-                                        <span className={styles.input_title}>staff number</span>
-                                        <Input className={styles.input} size="large" placeholder="20"/>
-                                    </Col>
-                                    <Col>
-                                        <span className={styles.input_title}>staff number</span>
-                                        <Input className={styles.input} size="large" placeholder="30"/>
-                                    </Col>
-                                </Row>
-                                <div className={styles.results}>
-                                    <div className={styles.results_title}>
-                                        <h1>Results</h1>
-                                        <hr/>
+class ROI extends React.Component {
+
+    state = {
+        consultants: "",
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Container className={styles.container}>
+                    <Row>
+                        <Col md={{span: 8, offset: 2}} className={styles.col_intro}>
+                            <h1>What could you save with SearchBase?</h1>
+                            <hr/>
+                            <h4>Use our super simple ROI calculator below to learn more</h4>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{span: 10, offset: 1}} className={styles.col_card}>
+                            <Card className={styles.card}>
+                                <Container>
+                                    <Row className={styles.row_input}>
+                                        <Col>
+                                            <span className={styles.input_title}>Consultant number</span>
+                                            <Input className={styles.input} size="large" placeholder="15"
+                                                   value={this.state.consultants}
+                                                   onChange={e => {
+                                                       if (isNaN(e.target.value))
+                                                           return;
+                                                       this.setState({consultants: e.target.value})
+                                                   }}/>
+                                        </Col>
+                                        <Col>
+                                            <span className={styles.input_title}>staff number</span>
+                                            <Input className={styles.input} size="large" placeholder="20"/>
+                                        </Col>
+                                        <Col>
+                                            <span className={styles.input_title}>staff number</span>
+                                            <Input className={styles.input} size="large" placeholder="30"/>
+                                        </Col>
+                                    </Row>
+                                    <div className={styles.results}>
+                                        <div className={styles.results_title_wrapper}>
+                                            <div className={styles.results_title}>
+                                                <hr/>
+                                                <h1>Results</h1>
+                                                <hr/>
+                                            </div>
+                                        </div>
+                                        <Row className={styles.row_result}>
+                                            <Col>
+                                                <ResultItem title="Revenue generated per yr." currency valSize="large"
+                                                            value={this.state.consultants * 3612}/>
+                                            </Col>
+                                        </Row>
+                                        <Row className={styles.row_result_text}>
+                                            <Col md={{span: 8, offset: 2}}>
+                                                <h1>So, per year the following is potential...</h1>
+                                            </Col>
+                                        </Row>
+                                        <Row className={styles.row_result}>
+                                            <Col>
+                                                <ResultItem title="Hours Saved per yr."
+                                                            value={this.state.consultants * 84}/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Productivity Pounds Saved per yr." value='0'/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Addt'l Revenue per yr." currency value='0'/>
+                                            </Col>
+                                        </Row>
+                                        <Row className={styles.row_result_text}>
+                                            <Col md={{span: 8, offset: 2}}>
+                                                <h1>...in other words, automating activities like
+                                                    NPS surveys, sales follow-up, candidate engagement and more
+                                                    equals...</h1>
+                                            </Col>
+                                        </Row>
+                                        <Row className={styles.row_result}>
+                                            <Col>
+                                                <ResultItem title="Hours Saved per yr."
+                                                            valSize="small"
+                                                            value={this.state.consultants * 84}/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Productivity Pounds Saved per yr."
+                                                            valSize="small" value='0'/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Addt'l Revenue per yr." valSize="small"
+                                                            currency
+                                                            value='0'/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Hours Saved per yr."
+                                                            valSize="small"
+                                                            value={this.state.consultants * 84}/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Productivity Pounds Saved per yr."
+                                                            valSize="small" value='0'/>
+                                            </Col>
+                                            <Col>
+                                                <ResultItem title="Addt'l Revenue per yr." valSize="small"
+                                                            currency
+                                                            value='0'/>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <Link to="/get-started">
+                                                    <Button className={styles.button} variant="outline-light">Book a
+                                                        demo</Button>
+                                                </Link>
+                                            </Col>
+                                        </Row>
                                     </div>
-                                    <Row className={styles.row_result}>
-                                        <Col>
-                                            <ResultItem title="Revenue generated per yr." currency valSize="large" value='20'/>
-                                        </Col>
-                                    </Row>
-                                    <Row className={styles.row_result}>
-                                        <Col>
-                                            <ResultItem title="Hours Saved per yr." value='20'/>
-                                        </Col>
-                                        <Col>
-                                            <ResultItem title="Productivity Pounds Saved per yr." value='20'/>
-                                        </Col>
-                                        <Col>
-                                            <ResultItem title="Addt'l Revenue per yr." currency value='20'/>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </Container>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </Layout>
-    );
-};
+                                </Container>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Layout>
+        );
+    }
+}
 
 export default ROI;
--- Client/src/application/Home/component/page/roi/result-item/ResultItem.js
+++ Client/src/application/Home/component/page/roi/result-item/ResultItem.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import styles from './result-item.module.css'
 import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMinus} from "@fortawesome/free-solid-svg-icons";
 
 const ResultItem = props => {
 
     let valSize = '2.5em';
+    let titleSize = '0.85em';
     let valFontWeight = 'normal';
+    let fontAwesomeSize = '2x';
+    let iconMargin = '.25em 0';
     switch (props.valSize) {
         case 'small':
             valSize = '1.5em';
             valFontWeight = 'lighter';
-            break;
-        case 'big':
-            valSize = '4em';
-            valFontWeight = 'bold';
+            fontAwesomeSize = "1x";
+            titleSize = '0.75em';
+            iconMargin = '0';
             break;
         case 'large':
             valSize = '6em';
             valFontWeight = 'bold';
+            fontAwesomeSize = "3x";
+            titleSize = '1em';
+            iconMargin = '.5em 0';
             break;
     }
 
+    let isZero = () => {
+        if (Number.isInteger(props.value))
+            return props.value === 0;
+        else
+            return props.value === "0";
+    };
+
     return (
-        <div>
-            <h2 className={styles.value} style={{fontSize: valSize, fontWeight: valFontWeight}}>{props.currency?'£':''}{props.value}</h2>
-            <h1 className={styles.title}>{props.title}</h1>
+        <div className={styles.wrapper}>
+            <div className={styles.value_wrapper}>
+            <span className={styles.value} style={isZero() ? {} : {fontSize: valSize, fontWeight: valFontWeight}}>
+                {isZero()
+                    ? <FontAwesomeIcon size={fontAwesomeSize} style={{margin: iconMargin}} className={styles.icon}
+                                       icon={faMinus}/>
+                    : (props.currency ? `£${props.value}` : `${props.value}`)
+                }
+            </span>
+            </div>
+            <h1 className={styles.title} style={{fontSize: titleSize}}>{props.title}</h1>
         </div>
     );
 };
@@ -33,7 +55,7 @@ ResultItem.propTypes = {
     value: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     currency: PropTypes.bool,
-    valSize: PropTypes.oneOf(["small", "big", "large"])
+    valSize: PropTypes.oneOf(["small", "default", "large"])
 };
 
 export default ResultItem;

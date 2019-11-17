aa6364a2581c570cd3b3a7e46008019851ca6e34
abdullah <sombek990@hotmail.com>
2019-11-18 00:44:15 +0300

    dynamic form items from json in quick build

--- Client/src/application/Dashboard/Assistants/Modals/QuickBuildModal.js
+++ Client/src/application/Dashboard/Assistants/Modals/QuickBuildModal.js
@@ -1,114 +1,128 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {Button, Form, Input, Modal} from 'antd';
-
-const FormItem = Form.Item;
-
+import { Button, Form, Input, Modal } from 'antd';
+import job_template from 'helpers/static_data/job-template';
 
 
 class NewAssistantModal extends Component {
 
-    state = {
-        isPopupDisabled: true,
-        isAlertsEnabled: false,
-        // alertOptions: {0: "Immediately", 4: "4 hours", 8: "8 hours", 12: "12 hours", 24: "24 hours"}
-        alertOptions: {0: "Immediately"}
-    };
-
     handleAdd = () => {
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                // this.props.addAssistant(values)
+                const { blocks } = job_template.flow.groups[0];
+
+                delete values.keys;
+                console.log(values);
+
+                blocks.forEach(block => {
+                    values.forEach(value => {
+                        if (block.Type !== 'Question')
+                            block.Content.text.replace(`$\{${value}\}$`, value);
+                    });
+
+                    if (block.Type === 'Question') {
+                        let form = block.Content.text.match(/\$\{.*?}\$/g);
+                        if (form)
+                            forms.push(...form);
+
+                        const answers = block.Content.answers;
+                        answers.forEach(answer => {
+                            form = answer.text.match(/\$\{.*?}\$/g);
+                            if (form)
+                                forms.push(...form);
+                        });
+                    }
+                });
             }
         });
     };
 
+    componentDidMount() {
+        let forms = [];
+        const { blocks } = job_template.flow.groups[0];
+        blocks.forEach(block => {
+            if (block.Type !== 'Question') {
+                const form = block.Content.text.match(/\$\{.*?}\$/g);
+                if (form)
+                    forms.push(...form);
+            }
+
+            if (block.Type === 'Question') {
+                let form = block.Content.text.match(/\$\{.*?}\$/g);
+                if (form)
+                    forms.push(...form);
+
+                const answers = block.Content.answers;
+                answers.forEach(answer => {
+                    form = answer.text.match(/\$\{.*?}\$/g);
+                    if (form)
+                        forms.push(...form);
+                });
+            }
+        });
+
+        forms = forms.map(form => {
+            form = form.replace('${', '');
+            form = form.replace('}$', '');
+            form = form.trim();
+            return form;
+        });
+
+        forms = forms.filter((v, i, a) => a.indexOf(v) === i);
+
+
+        const { form } = this.props;
+        forms.forEach(from => {
+            // can use data-binding to get
+            const keys = form.getFieldValue('keys');
+
+            const nextKeys = keys.concat(from);
+
+            // can use data-binding to set
+            // important! notify form to detect changes
+            form.setFieldsValue({
+                keys: nextKeys
+            });
+        });
+
+    }
 
 
     render() {
+        const { getFieldDecorator, getFieldValue } = this.props.form;
+        const { hideModal, visible } = this.props;
 
-        const {getFieldDecorator} = this.props.form;
-        const {hideModal, visible} = this.props;
+        getFieldDecorator('keys', { initialValue: [] });
+
+        const formItems = getFieldValue('keys').map(
+            (key, index) => (
+                <Form.Item key={index} label={key}>
+                    {getFieldDecorator(`${key}`, {
+                        rules: [{
+                            required: true,
+                            message: `Please input ${key}`
+                        }]
+                    })(
+                        <Input placeholder={`${key} value`}/>
+                    )}
+                </Form.Item>
+            )
+        );
 
         return (
-            <Modal
-                width={500}
-                title="Quickly create a new assistant"
-                visible={visible}
-                onCancel={hideModal}
-                destroyOnClose={true}
-                footer={[
-                    <Button key="cancel" onClick={hideModal}>Cancel</Button>,
-                    <Button key="submit" type="primary" onClick={this.handleAdd}>
-                        Build
-                    </Button>,
-                ]}>
+            <Modal width={500}
+                   title="Quickly create a new assistant"
+                   visible={visible}
+                   onCancel={hideModal}
+                   destroyOnClose={true}
+                   footer={[
+                       <Button key="cancel" onClick={hideModal}>Cancel</Button>,
+                       <Button key="submit" type="primary" onClick={this.handleAdd}>
+                           Build
+                       </Button>
+                   ]}>
                 <Form layout='vertical'>
-                    <FormItem
-                        label="Job salary"
-                        extra="Enter job salary">
-                        {getFieldDecorator('jobSalary', {
-                            initialValue: '',
-                            rules: [
-                                {whitespace: true, required: true, message: 'Please input your assistant name'},
-                                {validator: this.checkName}
-                            ]
-                        })(
-                            <Input placeholder="Ex: 50,000£"/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        label="Job Description"
-                        extra="Enter job description">
-                        {getFieldDecorator('jobDescription', {
-                            initialValue: '',
-                            rules: [
-                                {whitespace: true, required: true, message: 'Please input job description'},
-                                {validator: this.checkName}
-                            ]
-                        })(
-                            <Input placeholder=""/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        label="Benefits"
-                        extra="Enter a benefits">
-                        {getFieldDecorator('benefits', {
-                            initialValue: '',
-                            rules: [
-                                {whitespace: true, required: true, message: 'Please input benefits'},
-                                {validator: this.checkName}
-                            ]
-                        })(
-                            <Input placeholder=""/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        label="ًRequirements"
-                        extra="Enter requirements">
-                        {getFieldDecorator('requirements', {
-                            initialValue: '',
-                            rules: [
-                                {whitespace: true, required: true, message: 'Please input requirements'},
-                                {validator: this.checkName}
-                            ]
-                        })(
-                            <Input placeholder=""/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        label="Job Location"
-                        extra="Enter job location">
-                        {getFieldDecorator('jobLocation', {
-                            initialValue: '',
-                            rules: [
-                                {whitespace: true, required: true, message: 'Please input job location'},
-                                {validator: this.checkName}
-                            ]
-                        })(
-                            <Input placeholder="Ex: London, Manchester, Cardiff"/>
-                        )}
-                    </FormItem>
+                    {formItems}
                 </Form>
             </Modal>
         );
@@ -116,4 +130,4 @@ class NewAssistantModal extends Component {
 }
 
 
-export default Form.create()(NewAssistantModal)
+export default Form.create()(NewAssistantModal);

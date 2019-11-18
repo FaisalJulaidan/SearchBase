12aba50b770d0d64a506ed19494ff65357e069e4
abdullah <sombek990@hotmail.com>
2019-11-18 23:19:47 +0300

    Finalize quick build

--- Client/src/application/Dashboard/Assistants/Modals/QuickBuildModal.js
+++ Client/src/application/Dashboard/Assistants/Modals/QuickBuildModal.js
@@ -1,62 +1,77 @@
 import React, { Component } from 'react';
 
-import { Button, Form, Input, Modal } from 'antd';
-import job_template from 'helpers/static_data/job-template';
+import { Button, Divider, Form, Input, Modal } from 'antd';
+import { deepClone } from 'helpers/deepClone';
 
 
 class NewAssistantModal extends Component {
 
+
+    refrences = [];
+    job_template = {};
+
     handleAdd = () => {
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                const { blocks } = job_template.flow.groups[0];
-
-                delete values.keys;
-                console.log(values);
-
-                blocks.forEach(block => {
-                    values.forEach(value => {
-                        if (block.Type !== 'Question')
-                            block.Content.text.replace(`$\{${value}\}$`, value);
-                    });
-
-                    if (block.Type === 'Question') {
-                        let form = block.Content.text.match(/\$\{.*?}\$/g);
-                        if (form)
-                            forms.push(...form);
-
-                        const answers = block.Content.answers;
-                        answers.forEach(answer => {
-                            form = answer.text.match(/\$\{.*?}\$/g);
-                            if (form)
-                                forms.push(...form);
+                this.refrences.forEach(ref => {
+                    if (ref.Content?.text) // check if any of values is in the text, then replace it
+                        values.keys.forEach(key => {
+                            const tester = `\$\{${key}\}\$`;
+                            if (ref.Content.text.includes(tester))
+                                ref.Content.text = ref.Content.text.replace(tester, values[key]);
                         });
-                    }
+
+                    if (ref.text)
+                        values.keys.forEach(key => {
+                            const tester = `\$\{${key}\}\$`;
+
+                            if (ref.text.includes(tester))
+                                ref.text = ref.text.replace(tester, values[key]);
+                        });
                 });
+
+                const newAssistant = {
+                    assistantDesc: '',
+                    assistantName: values.assistantName,
+                    flow: this.job_template.flow,
+                    template: 'none',
+                    topBarText: values.topBarText,
+                    welcomeMessage: values.welcomeMessage
+                };
+                this.props.addAssistant(newAssistant);
+                return this.props.hideModal();
             }
         });
     };
 
-    componentDidMount() {
+    async componentDidMount() {
+        this.job_template = deepClone(await import('helpers/static_data/job-template'));
+
         let forms = [];
-        const { blocks } = job_template.flow.groups[0];
+        const { blocks } = this.job_template.flow.groups[0];
         blocks.forEach(block => {
             if (block.Type !== 'Question') {
                 const form = block.Content.text.match(/\$\{.*?}\$/g);
-                if (form)
+                if (form) {
+                    this.refrences.push(block);
                     forms.push(...form);
+                }
             }
 
             if (block.Type === 'Question') {
                 let form = block.Content.text.match(/\$\{.*?}\$/g);
-                if (form)
+                if (form) {
+                    this.refrences.push(block);
                     forms.push(...form);
+                }
 
                 const answers = block.Content.answers;
                 answers.forEach(answer => {
                     form = answer.text.match(/\$\{.*?}\$/g);
-                    if (form)
+                    if (form) {
+                        this.refrences.push(answer);
                         forms.push(...form);
+                    }
                 });
             }
         });
@@ -75,9 +90,7 @@ class NewAssistantModal extends Component {
         forms.forEach(from => {
             // can use data-binding to get
             const keys = form.getFieldValue('keys');
-
             const nextKeys = keys.concat(from);
-
             // can use data-binding to set
             // important! notify form to detect changes
             form.setFieldsValue({
@@ -86,6 +99,14 @@ class NewAssistantModal extends Component {
         });
 
     }
+
+    checkName = (rule, value, callback) => {
+        if (!this.props.isAssistantNameValid(value)) {
+            callback('Assistant name already exists');
+        } else {
+            callback();
+        }
+    };
 
 
     render() {
@@ -110,7 +131,7 @@ class NewAssistantModal extends Component {
         );
 
         return (
-            <Modal width={500}
+            <Modal width={600}
                    title="Quickly create a new assistant"
                    visible={visible}
                    onCancel={hideModal}
@@ -122,6 +143,55 @@ class NewAssistantModal extends Component {
                        </Button>
                    ]}>
                 <Form layout='vertical'>
+                    <Form.Item label="Assistant Name"
+                               extra="Enter a name for your assistant">
+                        {getFieldDecorator('assistantName', {
+                            initialValue: '',
+                            rules: [
+                                { whitespace: true, required: true, message: 'Please input your assistant name' },
+                                { validator: this.checkName }
+                            ]
+                        })(
+                            <Input placeholder="Ex: My first assistant, Sales Assistant"/>
+                        )}
+                    </Form.Item>
+
+                    <div style={{ display: 'flex' }}>
+                        <div style={{ width: '50%', padding: '0 10px 0 0' }}>
+                            <Form.Item label="Welcome Message"
+                                       extra="This will be sent as first message">
+                                {getFieldDecorator('welcomeMessage', {
+                                    rules: [{
+                                        whitespace: true, required: true, message: 'Please input your welcome message'
+                                    }]
+                                })(
+                                    <Input placeholder="Ex: Hey there, Welcome visitor"/>
+                                )}
+                            </Form.Item>
+                        </div>
+
+                        <div style={{ width: '50%' }}>
+                            <Form.Item label="Header Title"
+                                       extra="This will appear on top of your chatbot">
+                                {getFieldDecorator('topBarText', {
+                                    rules: [{
+                                        whitespace: true,
+                                        required: true,
+                                        message: 'Please input your header title'
+                                    }]
+                                })(
+                                    <Input placeholder="Ex: Recruiter Bot"/>
+                                )}
+                            </Form.Item>
+                        </div>
+                    </div>
+
+
+                    <Divider/>
+
+                    <h3>Template Filling</h3>
+                    <br/>
+
                     {formItems}
                 </Form>
             </Modal>
